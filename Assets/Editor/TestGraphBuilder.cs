5dfe07b73c40b5d9149c8f06b593ccef94ada26c
mikkoh <mikko.haapoja@shopify.com>
2016-12-02 17:15:43 -0500

    Added tests to check that different GraphQL types were generated

--- Assets/Editor/TestGraphBuilder.cs
+++ Assets/Editor/TestGraphBuilder.cs
@@ -1,10 +1,27 @@
 namespace Shopify.Tests
 {
+    using System;
+    using System.Linq;
     using NUnit.Framework;
     using Shopify.Unity;
 
     [TestFixture]
     public class TestGraphQLGenerator {
+        [Test]
+        public void generatedGraphQLEnums() {
+            Assert.IsTrue(DoesExist("CropRegion"));
+        }
+
+        [Test]
+        public void generatedGraphQLInterfaces() {
+            Assert.IsTrue(DoesExist("Node"));
+        }
+
+        [Test]
+        public void generatedGraphQLInputObjects() {
+            Assert.IsTrue(DoesExist("ApiCustomerAccessTokenCreateInputInput"));
+        }
+
         [Test]
         public void queryRootBuildsQuery() {
             QueryRootQuery query = Root.buildQuery();
@@ -19,7 +36,7 @@ namespace Shopify.Tests
         }
 
         [Test] 
-        public void inputTypeToString() {
+        public void inputObjectToString() {
             ApiCustomerAccessTokenCreateInputInput input = new ApiCustomerAccessTokenCreateInputInput(email: "email@email.com", password: "123456", clientMutationId: "333");
 
             Assert.AreEqual(
@@ -43,6 +60,13 @@ namespace Shopify.Tests
                 "mutation{apiCustomerAccessTokenCreate(input:{email:\"email@email.com\",password:\"123456\",clientMutationId:\"333\"}){apiCustomerAccessToken{accessToken},clientMutationId}}",
                 query.ToString()
             );
+        }
+
+        private bool DoesExist(string className) {
+            return (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+            from type in assembly.GetTypes()
+            where type.Name == className
+            select type).Any();
         }
     }    
 }
