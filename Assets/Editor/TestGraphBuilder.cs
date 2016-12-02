df859d8ca1207c9eb3b1581471acaaad0e6addfc
mikkoh <mikko.haapoja@shopify.com>
2016-12-02 15:18:53 -0500

    Added mutation test

--- Assets/Editor/TestGraphBuilder.cs
+++ Assets/Editor/TestGraphBuilder.cs
@@ -12,7 +12,27 @@ namespace Shopify.Tests
                 s.name()
             );
 
-            Assert.AreEqual(query.ToString(), "{shop{name}}");
+            Assert.AreEqual(
+                "{shop{name}}",
+                query.ToString()
+            );
+        }
+
+        [Test]
+        public void mutationRootBuildsQuery() {
+            MutationQuery query = Root.buildMutation()
+                .apiCustomerAccessTokenCreate(r => r
+                    .apiCustomerAccessToken(a => a
+                        .accessToken()
+                    )
+                    .clientMutationId(), 
+                    new ApiCustomerAccessTokenCreateInputInput(email: "email@email.com", password: "123456", clientMutationId: "333")
+                );
+
+            Assert.AreEqual(
+                "mutation{apiCustomerAccessTokenCreate(input:{email:\"email@email.com\",password:\"123456\",clientMutationId:\"333\"}){apiCustomerAccessToken{accessToken},clientMutationId}}",
+                query.ToString()
+            );
         }
     }    
 }
