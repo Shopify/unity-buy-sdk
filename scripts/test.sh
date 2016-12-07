ea221782a3e9ddde33eb4b41b074872548de9052
mikkoh <mikko.haapoja@shopify.com>
2016-12-07 12:35:31 -0500

    Added a check to not run tests if compiling tests failed

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,7 @@ which mcs &> /dev/null || die "Mono is not installed"
 which nunit-console &> /dev/null || die "nunit-console is not installed"
 
 mcs -recurse:'*.cs' -reference:nunit.framework.dll -target:library -out:test.dll
-nunit-console test.dll
+
+if [ $? = 0 ] ; then
+    nunit-console test.dll
+fi
