f5084e31a8fb415b0f0e0cff88e259a4c35c7367
mikkoh <mikko.haapoja@shopify.com>
2016-12-07 14:45:51 -0500

    Removed imports from scalar since we're not using it right now

--- scripts/generator/graphql_generator/csharp/scalar.rb
+++ scripts/generator/graphql_generator/csharp/scalar.rb
@@ -3,11 +3,10 @@ module GraphQLGenerator
     class Scalar
       attr_reader :csharp_type, :nullable_csharp_type, :graph_type, :imports
 
-      def initialize(graph_type:, csharp_type:, nullable_csharp_type:, imports: [])
+      def initialize(graph_type:, csharp_type:, nullable_csharp_type:)
         @graph_type = graph_type
         @csharp_type = csharp_type
         @nullable_csharp_type = nullable_csharp_type
-        @imports = imports
       end
     end
   end
