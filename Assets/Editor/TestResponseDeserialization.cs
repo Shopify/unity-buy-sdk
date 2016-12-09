ed6024d2927ae43b2adfb331bd5ef4816c4454f4
mikkoh <mikko.haapoja@shopify.com>
2016-12-09 14:41:38 -0500

    Implemented deserializing enums

--- Assets/Editor/TestResponseDeserialization.cs
+++ Assets/Editor/TestResponseDeserialization.cs
@@ -6,7 +6,7 @@ namespace Shopify.Tests {
     [TestFixture]
     public class TestResponseDeserialization {
         [Test]
-        public void CanDeserializeResponse() {
+        public void CanDeserializeBasic() {
             string shopName = "test-shop";
 
             Dictionary<string,object> dataJSONShop = new Dictionary<string,object>() {
@@ -24,6 +24,48 @@ namespace Shopify.Tests {
             QueryRoot response = new QueryRoot(dataJSON);
 
             Assert.AreEqual(shopName, response.shop.name);
+        }
+
+        [Test]
+        public void CanDeserializeEnum() {
+            string shopName = "test-shop";
+
+            Dictionary<string,object> dataJSONShop = new Dictionary<string,object>() {
+                { "currencyCode", "CAD" }   
+            }; 
+
+            Dictionary<string,object> dataJSONQueryRoot = new Dictionary<string,object>() {
+                { "shop", dataJSONShop }   
+            };            
+
+            Dictionary<string,object> dataJSON = new Dictionary<string,object>() {
+                { "data", dataJSONQueryRoot }   
+            }; 
+
+            QueryRoot response = new QueryRoot(dataJSON);
+
+            Assert.AreEqual(CurrencyCode.CAD, response.shop.currencyCode);
+        }
+
+        [Test]
+        public void WillReturnUnkownEnum() {
+            string shopName = "test-shop";
+
+            Dictionary<string,object> dataJSONShop = new Dictionary<string,object>() {
+                { "currencyCode", "DOESNT EXIST" }   
+            }; 
+
+            Dictionary<string,object> dataJSONQueryRoot = new Dictionary<string,object>() {
+                { "shop", dataJSONShop }   
+            };            
+
+            Dictionary<string,object> dataJSON = new Dictionary<string,object>() {
+                { "data", dataJSONQueryRoot }   
+            }; 
+
+            QueryRoot response = new QueryRoot(dataJSON);
+
+            Assert.AreEqual(CurrencyCode.UNKNOWN, response.shop.currencyCode);
         }
     }
 }
