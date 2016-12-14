0cf933439351cc88c99686eb55b4a7d4923dd6f6
mikkoh <mikko.haapoja@shopify.com>
2016-12-14 10:04:59 -0500

    Defining variables via circle.yml

--- scripts/install_unity.sh
+++ scripts/install_unity.sh
@@ -1,8 +1,9 @@
 . $(dirname $0)/common.sh
 
-UNITY_DOWNLOAD_DIR="`pwd`/unity"
-UNITY_PKG_LOCATION=$UNITY_DOWNLOAD_DIR/Unity.pkg
-UNITY_PKG_URL=http://netstorage.unity3d.com/unity/fdbb5133b820/MacEditorInstaller/Unity-5.3.4f1.pkg
+# set UNITY_DOWNLOAD_DIR if it hasn't been set. It may have been set from 
+UNITY_DOWNLOAD_DIR="${UNITY_DOWNLOAD_DIR:-`pwd`/unity}"
+UNITY_PKG_LOCATION=${UNITY_PKG_LOCATION:-$UNITY_DOWNLOAD_DIR/Unity.pkg}
+UNITY_PKG_URL=${UNITY_PKG_URL:-http://netstorage.unity3d.com/unity/fdbb5133b820/MacEditorInstaller/Unity-5.3.4f1.pkg}
 
 if [ ! -e "$UNITY_PKG_LOCATION" ]; then
     out "Downloading Unity to $UNITY_DOWNLOAD_DIR"
