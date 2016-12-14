901ffcb6f3945e060cc5595887e33310d4e312be
mikkoh <mikko.haapoja@shopify.com>
2016-12-14 10:34:36 -0500

    Setup xsltproc to convert nunit xml output to junit xml output

--- scripts/test_unity.sh
+++ scripts/test_unity.sh
@@ -5,6 +5,14 @@ UNITY_LOG_PATH=$(pwd)/test.log
 
 which $UNITY_PATH &> /dev/null || die "Unity does not exist at $UNITY_PATH" 
 
+convertNUnitToJUnit() {
+    xmlOutPath=$1 
+
+    if $CIRCLECI; then
+        mkdir $UNITY_CIRCLE_XML_PATH
+        xsltproc -o $xmlOutPath $SCRIPTS_ROOT/nunit-to-junit.xsl EditorTestResults.xml
+    fi
+}
 
 $UNITY_PATH \
     -batchmode \
@@ -18,12 +26,14 @@ $UNITY_PATH \
 
 if [ $? = 0 ] ; then
     echo "Tests passed"
+    convertNUnitToJUnit $UNITY_CIRCLE_XML_OUT_PATH
     exit 0
 else
     echo "Tests failed. Exited with $?"
     echo "------------------\n\n"
     if [ -e "EditorTestResults.xml" ]; then
         cat EditorTestResults.xml
+        convertNUnitToJUnit $UNITY_CIRCLE_XML_OUT_PATH
     else
         cat $UNITY_LOG_PATH 
     fi
