bf4f746b5659dd3baac576369b2c90703b09271a
mikkoh <mikko.haapoja@shopify.com>
2017-01-18 12:34:52 -0500

    Connections now have AddFromConnection

--- Assets/Editor/TestRelay.cs
+++ Assets/Editor/TestRelay.cs
@@ -7,34 +7,78 @@ namespace Shopify.Tests {
     [TestFixture]
     public class TestRelay {
         [Test]
-        public void TestCastConnectionToList() {
-            string stringJSON = @"{
-                ""edges"": [ 
+        public void TestAddingToConnection() {
+            string responseString1 = @"{
+                ""edges"": [
                     {
                         ""node"": {
-                            ""title"": ""Product 1""
-                        }
-                    },
-                    {
-                        ""node"": {
-                            ""title"": ""Product 2""
-                        }
+                            ""title"": ""product1""
+                        },
+                        ""cursor"": ""0""
                     }
-                ]
+                ],
+                ""pageInfo"": {
+                    ""hasNextPage"": true
+                }
             }";
 
-            ProductConnection connection = new ProductConnection((Dictionary<string, object>) Json.Deserialize(stringJSON));
-            List<Product> products = (List<Product>) connection;
-            bool couldIterateOverConnection = false;
+            string responseString2 = @"{
+                ""edges"": [
+                    {
+                        ""node"": {
+                            ""title"": ""product2""
+                        },
+                        ""cursor"": ""1""
+                    }
+                ],
+                ""pageInfo"": {
+                    ""hasNextPage"": false
+                }
+            }";
 
-            foreach (Product product in products) {
-                couldIterateOverConnection = true;
-            }
+            Dictionary<string, object> response1 = (Dictionary<string, object>) Json.Deserialize(responseString1);
+            Dictionary<string, object> response2 = (Dictionary<string, object>) Json.Deserialize(responseString2);
 
-            Assert.IsNotNull(products, "products is not null");
-            Assert.AreEqual(2, products.Count, "products list has two products");
-            Assert.AreEqual("Product 2", products[1].title(), "List had data");
-            Assert.IsTrue(couldIterateOverConnection, "could iterate over connection");
+            ProductConnection queryResponse1 = new ProductConnection(response1);
+            ProductConnection queryResponse2 = new ProductConnection(response2);
+            queryResponse1.AddFromConnection(queryResponse2);
+
+            Assert.AreEqual(2, queryResponse1.edges().Count);
+            Assert.AreEqual("product1", queryResponse1.edges()[0].node().title());
+            Assert.AreEqual("product2", queryResponse1.edges()[1].node().title());
+            Assert.AreEqual(false, queryResponse1.pageInfo().hasNextPage());
+
+            // the following are to test whether original response was not mutated
+            Assert.AreEqual(1, ((List<object>) response1["edges"]).Count);
+            Assert.AreEqual("product1", ((Dictionary<string,object>) ((Dictionary<string,object>) ((List<object>) response1["edges"])[0])["node"])["title"]);
+        }
+
+        [Test]
+        public void TestAddingToConnectionUnitialized() {
+            string responseString2 = @"{
+                ""edges"": [
+                    {
+                        ""node"": {
+                            ""title"": ""product1""
+                        },
+                        ""cursor"": ""0""
+                    }
+                ],
+                ""pageInfo"": {
+                    ""hasNextPage"": true
+                }
+            }";
+            Dictionary<string, object> response2 = (Dictionary<string, object>) Json.Deserialize(responseString2);
+
+            Dictionary<string,object> response1 = new Dictionary<string,object>();
+            ProductConnection queryResponse1 = new ProductConnection(response1);
+            ProductConnection queryResponse2 = new ProductConnection(response2);
+
+            queryResponse1.AddFromConnection(queryResponse2);
+
+            Assert.AreEqual(1, queryResponse1.edges().Count);
+            Assert.AreEqual("product1", queryResponse1.edges()[0].node().title());
+            Assert.AreEqual(true, queryResponse1.pageInfo().hasNextPage());
         }
     }
 }
