5b32ab766caec2c0afc8cbe07c0aaeb84c55a260
Mikko Haapoja <mikkoh@users.noreply.github.com>
2017-01-19 12:58:17 -0500

    [Feature] Query Loader (#8)
    * Implemented a class QueryLoader which receives an object which performs network communciation. Also created QueryResponse and MutationResponse which inherit from TopLevelResponse
    
    * Fixed deserializing responses in tests
    
    * Renamed QueryResponseCallback to QueryResponseDelegate to match rest of library. Same for mutations
    
    * Fixed UnityLoader to be able to use WWW standalone by creating a static gameobject
    
    * Added integration test for UnityLoader
    
    * Fixed the request body for the UnityLoader to be just graphql query instead of a json payload
    
    * Updated UnityLoader to not destroy LoaderComponent
    
    * Added in UnitTestTools

---
+++ Assets/IntegrationTests/TestUnityLoader.cs
@@ -0,0 +1,29 @@
+namespace Shopify.Unity.Tests
+{
+    using UnityEngine;
+    using Shopify.Unity.SDK;
+
+    public class TestQueries {
+        public static QueryRootQuery Query = new QueryRootQuery().shop(shop => shop.name());
+    }
+
+    [IntegrationTest.DynamicTest ("TestScene")]
+    [IntegrationTest.SucceedWithAssertions]
+    public class TestUnityLoader : MonoBehaviour {
+        void Start() {
+            string domain = "graphql.myshopify.com";
+            string authorization = "MzUxYzEyMjAxN2QwZjJhOTU3ZDMyYWU3MjhhZDc0OWM=";
+
+            QueryLoader queryLoader = new QueryLoader(new UnityLoader(domain, authorization));
+
+            queryLoader.Query(TestQueries.Query, (QueryResponse response) => {
+                IntegrationTest.Assert(response.httpError == null, "http errors were not null: " + response.httpError);
+                IntegrationTest.Assert(response.errors == null, "GraphQL errors were null");
+                IntegrationTest.Assert(response.data != null, "Received a response");
+                IntegrationTest.Assert(response.data.shop().name() != null, "graphql");
+
+                IntegrationTest.Pass();
+            });
+        }
+    }   
+}
