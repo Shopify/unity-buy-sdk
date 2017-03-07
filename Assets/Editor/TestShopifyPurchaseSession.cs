2a7f0a5ba32e799a399a9e3757be3bf42237fd98
mikkoh <mikko.haapoja@shopify.com>
2017-03-07 11:46:18 -0500

    Added tests for adding and removing line items

--- Assets/Editor/TestShopifyPurchaseSession.cs
+++ Assets/Editor/TestShopifyPurchaseSession.cs
@@ -11,9 +11,35 @@ namespace Shopify.Tests
         public void TestGenericQuery() {
             ShopifyBuy.Init(new MockLoader());
 
-            PurchaseSession purchaseSession = ShopifyBuy.Client().PurchaseSession();
+            Shopify.Unity.PurchaseSession purchaseSession = ShopifyBuy.Client().PurchaseSession();
 
             Assert.IsNotNull(purchaseSession, "created a purchase session");
+        }
+
+        [Test]
+        public void TestAddRemoveLineItems() {
+            ShopifyBuy.Init(new MockLoader());
+
+            PurchaseSession purchaseSession = ShopifyBuy.Client().PurchaseSession();
+
+            Dictionary<string,object> data = new Dictionary<string,object>();
+            data.Add("id", "gid://shopify/ProductVariant/20756129347");
+
+            Shopify.Unity.GraphQL.ProductVariant variant = new Shopify.Unity.GraphQL.ProductVariant(data);
+
+            purchaseSession.LineItems.Set("gid://shopify/ProductVariant/20756129155", 33);
+            purchaseSession.LineItems.Set(variant, 2);
+
+            Assert.AreEqual(2, purchaseSession.LineItems.GetAll().Count);
+            Assert.AreEqual(33, purchaseSession.LineItems.Get("gid://shopify/ProductVariant/20756129155").quantity);
+            Assert.AreEqual(2, purchaseSession.LineItems.Get("gid://shopify/ProductVariant/20756129347").quantity);
+
+            bool didDelete = purchaseSession.LineItems.Delete("gid://shopify/ProductVariant/20756129155");
+            Assert.AreEqual(1, purchaseSession.LineItems.GetAll().Count);
+            Assert.IsTrue(didDelete);
+
+            didDelete = purchaseSession.LineItems.Delete("gid://shopify/ProductVariant/iamnotreal");
+            Assert.IsFalse(didDelete);
         }
 
         [Test]
