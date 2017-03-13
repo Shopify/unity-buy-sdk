179220b8e015633f86266812208e25bf024f1d0a
mikkoh <mikko.haapoja@shopify.com>
2017-03-13 14:00:21 -0400

    Updated Unity version

--- scripts/install_unity.sh
+++ scripts/install_unity.sh
@@ -3,7 +3,7 @@
 # set UNITY_DOWNLOAD_DIR if it hasn't been set. It may have been set from 
 UNITY_DOWNLOAD_DIR="${UNITY_DOWNLOAD_DIR:-`pwd`/unity}"
 UNITY_PKG_LOCATION=${UNITY_PKG_LOCATION:-$UNITY_DOWNLOAD_DIR/Unity.pkg}
-UNITY_PKG_URL=${UNITY_PKG_URL:-http://netstorage.unity3d.com/unity/fdbb5133b820/MacEditorInstaller/Unity-5.3.4f1.pkg}
+UNITY_PKG_URL=${UNITY_PKG_URL:-http://netstorage.unity3d.com/unity/3829d7f588f3/MacEditorInstaller/Unity-5.5.2f1.pkg}
 
 if [ ! -e "$UNITY_PKG_LOCATION" ]; then
     out "Downloading Unity to $UNITY_DOWNLOAD_DIR"
