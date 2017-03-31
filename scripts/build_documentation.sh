8748300565f48457b7a266c9b0f4bffec6fe2b78
mikkoh <mikko.haapoja@shopify.com>
2017-03-31 10:51:43 -0400

    Examples are now main page for doxygen generated comments

--- scripts/build_documentation.sh
+++ scripts/build_documentation.sh
@@ -2,7 +2,10 @@
 
 which doxygen &> /dev/null || die "doxygen is not installed"
 
+# Assets/Shopify/examples.md will be the main page for Doxygen
+cp Assets/Shopify/examples.txt Assets/Shopify/examples.md
 doxygen Doxyfile
+rm Assets/Shopify/examples.md
 
 if [ $? = 0 ] ; then
     echo "🦄  Generated documentation 🦄"
