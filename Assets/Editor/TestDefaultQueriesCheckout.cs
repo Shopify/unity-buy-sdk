0f157ef074a9529fc3b066ab8c21fa8ef20893b0
mikkoh <mikko.haapoja@shopify.com>
2017-05-16 10:48:04 -0400

    Added default queries for checkout

---
+++ Assets/Editor/TestDefaultQueriesCheckout.cs
@@ -0,0 +1,79 @@
+namespace Shopify.Tests
+{
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using Shopify.Unity;
+    using Shopify.Unity.SDK;
+    using Shopify.Unity.GraphQL;
+
+    [TestFixture]
+    public class TestDefaultQueries {
+        [Test]
+        public void TestCheckoutCreate() {
+            MutationQuery query = new MutationQuery();
+            List<CheckoutLineItemInput> lineItems = new List<CheckoutLineItemInput>();
+
+            DefaultQueries.checkout.Create(query, lineItems);
+
+            Assert.AreEqual(
+                "mutation{checkoutCreate (input:{lineItems:[]}){checkout {id webUrl ready lineItems (first:250){edges {node {id variant {id }}cursor }}}userErrors {field message }}}", 
+                query.ToString()
+            );
+        }
+
+        [Test]
+        public void TestCheckoutPoll() {
+            QueryRootQuery query = new QueryRootQuery();
+            string checkoutId = "an-id";
+            
+            DefaultQueries.checkout.Poll(query, checkoutId);
+
+            Assert.AreEqual(
+                "{node (id:\"an-id\"){__typename ...on Checkout{id webUrl ready }}}", 
+                query.ToString()
+            );
+        }
+
+        [Test]
+        public void TestCheckoutLineItemsAdd() {
+            MutationQuery query = new MutationQuery();
+            string checkoutId = "an-id";
+            List<CheckoutLineItemInput> lineItems = new List<CheckoutLineItemInput>();
+
+            DefaultQueries.checkout.LineItemsAdd(query, checkoutId, lineItems);
+
+            Assert.AreEqual(
+                "mutation{checkoutLineItemsAdd (checkoutId:\"an-id\",lineItems:[]){checkout {id webUrl ready lineItems (first:250){edges {node {id variant {id }}cursor }}}userErrors {field message }}}", 
+                query.ToString()
+            );
+        }
+
+        [Test]
+        public void TestCheckoutLineItemsRemove() {
+            MutationQuery query = new MutationQuery();
+            string checkoutId = "an-id";
+            List<string> lineItemIds = new List<string>();
+
+            DefaultQueries.checkout.LineItemsRemove(query, checkoutId, lineItemIds);
+
+            Assert.AreEqual(
+                "mutation{checkoutLineItemsRemove (checkoutId:\"an-id\",lineItemIds:[]){checkout {id webUrl ready lineItems (first:250){edges {node {id variant {id }}cursor }}}userErrors {field message }}}", 
+                query.ToString()
+            );
+        }
+
+        [Test]
+        public void TestCheckoutLineItemsUpdate() {
+            MutationQuery query = new MutationQuery();
+            string checkoutId = "an-id";
+            List<CheckoutLineItemUpdateInput> lineItems = new List<CheckoutLineItemUpdateInput>();
+
+            DefaultQueries.checkout.LineItemsUpdate(query, checkoutId, lineItems);
+
+            Assert.AreEqual(
+                "mutation{checkoutLineItemsUpdate (checkoutId:\"an-id\",lineItems:[]){checkout {id webUrl ready lineItems (first:250){edges {node {id variant {id }}cursor }}}userErrors {field message }}}", 
+                query.ToString()
+            );
+        }
+    }
+}
