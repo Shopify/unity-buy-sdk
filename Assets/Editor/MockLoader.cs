4a5fec6d17920af68cb1cfc0e1b0d667dba46620
mikkoh <mikko.haapoja@shopify.com>
2017-05-24 14:26:10 -0400

    Broke out products queries into it's own class

--- Assets/Editor/MockLoader.cs
+++ Assets/Editor/MockLoader.cs
@@ -5,13 +5,13 @@ namespace Shopify.Tests {
     using Shopify.Unity;
     using Shopify.Unity.GraphQL;
     using Shopify.Unity.SDK;
-
+    
     public class MockLoader : ILoader {
         private static bool Initialized;
-
+        
         public static int PageSize = DefaultQueries.MaxPageSize;
         private static List<IMockLoader> _Loaders;
-
+        
         private static void Initialize() {
             _Loaders = new List<IMockLoader>() {
                 new MockLoaderErrors(),
@@ -19,28 +19,28 @@ namespace Shopify.Tests {
                 new MockLoaderGeneric(),
                 new MockLoaderCollections()
             };
-
+            
             Initialized = true;
         }
-
+        
         public string Domain {
             get {
                 return "graphql.myshopify.com";
             }
         }
-
+        
         public string AccessToken {
             get {
                 return "1234";
             }
         }
-
+        
         public List<IMockLoader> Loaders {
             get {
                 return _Loaders;
             }
         }
-
+        
         public MockLoader() {
             if (!Initialized) {
                 Initialize();
@@ -49,16 +49,16 @@ namespace Shopify.Tests {
         
         public void Load(string query, LoaderResponseHandler callback) {
             bool handledResponse = false;
-
+            
             foreach(IMockLoader loader in _Loaders) {
                 if (loader.DoesHandleQueryResponse(query)) {
-                    loader.HandleResponse(query, callback);                    
+                    loader.HandleResponse(query, callback);
                     handledResponse = true;
-
+                    
                     break;
                 }
             }
-
+            
             if (!handledResponse) {
                 throw new Exception("NO QUERY RESPONSE: \n\n" + query + "\n\n");
             }
