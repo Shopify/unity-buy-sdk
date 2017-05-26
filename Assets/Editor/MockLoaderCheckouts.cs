67804103ad44558502342e07a964b0f1ba2cbc58
mikkoh <mikko.haapoja@shopify.com>
2017-05-26 14:14:59 -0400

    CheckoutMockLoader now handles a case where there's been an add, modify, and delete at the same time

--- Assets/Editor/MockLoaderCheckouts.cs
+++ Assets/Editor/MockLoaderCheckouts.cs
@@ -1,5 +1,6 @@
 namespace Shopify.Tests {
     public class MockLoaderCheckouts : BaseMockLoader {
+        // The following query is for a straightforward checkout create
         private const string CREATE_QUERY = @"mutation{checkoutCreate (input:{lineItems:[{quantity:33,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTE1NQ==""},{quantity:2,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTM0Nw==""}]}){checkout {id webUrl ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
 
         // The following two queries are used for a test where first a create is called and we return false for ready which forces a poll
@@ -7,9 +8,14 @@ namespace Shopify.Tests {
         private const string CREATE_QUERY_THAT_POLLS = @"mutation{checkoutCreate (input:{lineItems:[{quantity:33,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTPOLL==""},{quantity:2,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTM0Nw==""}]}){checkout {id webUrl ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
         private const string POLL_QUERY_AFTER_CREATE = @"{node (id:""checkout-id-poll""){__typename ...on Checkout{id webUrl ready }}}";
 
+        // The following queries are used to test creating a checkout, then modififying line items and getting a new url
+        private const string CREATE_QUERY_THAT_WILL_BE_UPDATED = @"mutation{checkoutCreate (input:{lineItems:[{quantity:33,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate==""},{quantity:2,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyDelete==""}]}){checkout {id webUrl ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
+        private const string ADD_UPDATE_DELETE_AFTER_CREATE = @"mutation{checkoutLineItemsAdd (lineItems:[{quantity:3,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NTMNewItem==""}],checkoutId:""checkout-id-poll""){checkout {id webUrl ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}checkoutLineItemsUpdate (checkoutId:""checkout-id-poll"",lineItems:[{id:""line-item-id1"",quantity:10,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate==""}]){checkout {id webUrl ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}checkoutLineItemsRemove (checkoutId:""checkout-id-poll"",lineItemIds:[""line-item-id2""]){checkout {id webUrl ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
+
         public MockLoaderCheckouts() {
             SetupCreateResponse();
             SetupCreateResponsesForPolling();
+            SetupCreateThenUpdateResponse();
         }
 
         private void SetupCreateResponse() {
@@ -106,6 +112,156 @@ namespace Shopify.Tests {
                             ""id"": ""checkout-id-poll"", 
                             ""webUrl"": ""http://shopify.com/checkout-with-poll-after-poll"", 
                             ""ready"": true
+                        }
+                    }
+                }"
+            );
+        }
+
+        private void SetupCreateThenUpdateResponse() {
+            AddResponse(
+                CREATE_QUERY_THAT_WILL_BE_UPDATED,
+                @"{
+                    ""data"": {
+                        ""checkoutCreate"": {
+                            ""checkout"": {
+                                ""id"": ""checkout-id-poll"", 
+                                ""webUrl"": ""http://shopify.com/checkout-create-before-update"",
+                                ""ready"": true,
+                                ""lineItems"": {
+                                    ""edges"": [
+                                        {
+                                            ""node"": {
+                                                ""id"": ""line-item-id1"", 
+                                                ""variant"": {
+                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate==""
+                                                }
+                                            },
+                                            ""cursor"": ""line-item-1-cursor""
+                                        },
+                                        {
+                                            ""node"": {
+                                                ""id"": ""line-item-id2"", 
+                                                ""variant"": {
+                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyDelete==""
+                                                }
+                                            },
+                                            ""cursor"": ""line-item-1-cursor""
+                                        } 
+                                    ],
+                                    ""pageInfo"": {
+                                        ""hasNextPage"": false
+                                    }
+                                }
+                            }, 
+                            ""userErrors"": []
+                        }
+                    }
+                }"
+            );
+            
+            AddResponse(
+                ADD_UPDATE_DELETE_AFTER_CREATE,
+                @"{
+                    ""data"": {
+                        ""checkoutLineItemsAdd"": {
+                            ""checkout"": {
+                                ""id"": ""checkout-id"", 
+                                ""webUrl"": ""http://shopify.com/checkout-create-after-update"",
+                                ""ready"": true,
+                                ""lineItems"": {
+                                    ""edges"": [
+                                        {
+                                            ""node"": {
+                                                ""id"": ""line-item-id1"", 
+                                                ""variant"": {
+                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate==""
+                                                }
+                                            },
+                                            ""cursor"": ""line-item-1-cursor""
+                                        },
+                                        {
+                                            ""node"": {
+                                                ""id"": ""line-item-id3"", 
+                                                ""variant"": {
+                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NTMNewItem==""
+                                                }
+                                            },
+                                            ""cursor"": ""line-item-3-cursor""
+                                        } 
+                                    ],
+                                    ""pageInfo"": {
+                                        ""hasNextPage"": false 
+                                    }
+                                }
+                            },
+                            ""userErrors"": []
+                        },
+                        ""checkoutLineItemsUpdate"": {
+                            ""checkout"": {
+                                ""id"": ""checkout-id"", 
+                                ""webUrl"": ""http://shopify.com/checkout-create-after-update"",
+                                ""ready"": true,
+                                ""lineItems"": {
+                                    ""edges"": [
+                                        {
+                                            ""node"": {
+                                                ""id"": ""line-item-id1"", 
+                                                ""variant"": {
+                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate==""
+                                                }
+                                            },
+                                            ""cursor"": ""line-item-1-cursor""
+                                        },
+                                        {
+                                            ""node"": {
+                                                ""id"": ""line-item-id3"", 
+                                                ""variant"": {
+                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NTMNewItem==""
+                                                }
+                                            },
+                                            ""cursor"": ""line-item-3-cursor""
+                                        } 
+                                    ],
+                                    ""pageInfo"": {
+                                        ""hasNextPage"": false 
+                                    }
+                                }
+                            },
+                            ""userErrors"": []
+                        },
+                        ""checkoutLineItemsRemove"": {
+                            ""checkout"": {
+                                ""id"": ""checkout-id"", 
+                                ""webUrl"": ""http://shopify.com/checkout-create-after-update"",
+                                ""ready"": true,
+                                ""lineItems"": {
+                                    ""edges"": [
+                                        {
+                                            ""node"": {
+                                                ""id"": ""line-item-id1"", 
+                                                ""variant"": {
+                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate==""
+                                                }
+                                            },
+                                            ""cursor"": ""line-item-1-cursor""
+                                        },
+                                        {
+                                            ""node"": {
+                                                ""id"": ""line-item-id3"", 
+                                                ""variant"": {
+                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NTMNewItem==""
+                                                }
+                                            },
+                                            ""cursor"": ""line-item-3-cursor""
+                                        } 
+                                    ],
+                                    ""pageInfo"": {
+                                        ""hasNextPage"": false 
+                                    }
+                                }
+                            },
+                            ""userErrors"": []
                         }
                     }
                 }"
