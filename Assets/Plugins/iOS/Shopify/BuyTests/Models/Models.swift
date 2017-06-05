6daecb670fb71f3ba771895189404b1a933e3a1a
Jeffrey Deng <jeffreydeng@live.com>
2017-06-05 11:23:40 -0400

    Updated copyright notice

--- Assets/Plugins/iOS/Shopify/BuyTests/Models/Models.swift
+++ Assets/Plugins/iOS/Shopify/BuyTests/Models/Models.swift
@@ -1,9 +1,9 @@
 //
 //  Models.swift
-//  Unity-iPhone
+//  UnityBuySDK
 //
 //  Created by Shopify.
-//
+//  Copyright (c) 2017 Shopify Inc. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
