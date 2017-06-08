eb47376a009b701ce6e25233158382b327772364
mikkoh <mikko.haapoja@shopify.com>
2017-06-08 13:49:51 -0400

    Implemented merging lists

--- Assets/Editor/TestResponeMergeUtil.cs
+++ Assets/Editor/TestResponeMergeUtil.cs
@@ -2,6 +2,7 @@ namespace Shopify.Tests
 {
     using NUnit.Framework;
     using Shopify.Unity.SDK;
+    using System.Collections;
     using System.Collections.Generic;
 
     [TestFixture]
@@ -39,6 +40,38 @@ namespace Shopify.Tests
             Assert.AreEqual("i am b", merged["fieldInBoth"], "Overwrote field in A from B");
             Assert.AreNotEqual(responseA["fieldHasDictionary"], merged["fieldHasDictionary"]);
             Assert.AreNotEqual(responseB["fieldHasDictionary"], merged["fieldHasDictionary"]);
+        }
+
+        [Test]
+        public void CustomFieldMerger() {
+            Dictionary<string, object> responseA = GetResponseA();
+            Dictionary<string, object> responseB = GetResponseB();
+
+            ResponseMergeUtil merger = new ResponseMergeUtil();
+
+            merger.AddFieldMerger("fieldInBoth", (string field, IDictionary into, IDictionary dataA, IDictionary dataB) => {
+                into[field] = (string) dataA[field] + (string) dataB[field];
+            });
+
+            Dictionary<string, object> merged = merger.Merge(responseA, responseB);
+
+            Assert.AreEqual("i am ai am b", merged["fieldInBoth"], "Custom merger worked");
+        }
+
+        [Test]
+        public void MergeListsConcat() {
+            Dictionary<string, object> responseA = GetResponseA();
+            Dictionary<string, object> responseB = GetResponseB();
+
+            ResponseMergeUtil merger = new ResponseMergeUtil();
+
+            merger.AddFieldMerger("fieldHasList", ResponseMergeUtil.MergeListsConcat);
+
+            Dictionary<string, object> merged = merger.Merge(responseA, responseB);
+
+            Assert.AreEqual(2, ((List<object>) merged["fieldHasList"]).Count, "Has two elements");
+            Assert.AreEqual(0, ((List<object>) merged["fieldHasList"])[0], "First value was correct");
+            Assert.AreEqual(1, ((List<object>) merged["fieldHasList"])[1], "Second value was correct");
         }
 
         private Dictionary<string,object> GetResponseA() {
