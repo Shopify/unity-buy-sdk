3dc548b3826623acc061a02f71e41eafcdf449f7
Jeffrey Deng <jeffreydeng@live.com>
2017-06-22 15:38:48 -0400

    [Feature] iOS Cart Apple Pay (#104)
    * Added Cart Extension for Apple Pay
    * Refactored parsing into separate method

---
+++ Assets/Plugins/iOS/Shopify/Cart+ApplePay.mm
@@ -0,0 +1,56 @@
+//
+//  Cart+ApplePay.mm
+//  UnityBuySDK
+//
+//  Created by Shopify.
+//  Copyright © 2017 Shopify Inc. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "Cart+ApplePay.h"
+#import <PassKit/PassKit.h>
+#import "ProductName-Swift.h"
+
+bool _CanCheckoutWithApplePay() {
+    return [PaymentSession canMakePayments];
+}
+
+bool _CanShowApplePaySetup() {
+    return [PaymentSession canShowSetup];
+}
+
+void _ShowApplePaySetup() {
+    [PaymentSession showSetup];
+}
+
+bool _CreateApplePaySession(const char *unityDelegateObjectName, const char *merchantID, const char *countryCode, const char *currencyCode, const char *serializedSummaryItems, const char *serializedShippingMethods, bool requiringShipping) {
+
+    return [Cart createApplePaySessionWithUnityDelegateObjectName:[NSString stringWithUTF8String:unityDelegateObjectName]
+                                                       merchantID:[NSString stringWithUTF8String:merchantID]
+                                                      countryCode:[NSString stringWithUTF8String:countryCode]
+                                                     currencyCode:[NSString stringWithUTF8String:currencyCode]
+                                           serializedSummaryItems:[NSString stringWithUTF8String:serializedSummaryItems]
+                                        serializedShippingMethods:[NSString stringWithUTF8String:serializedShippingMethods]
+                                                requiringShipping:requiringShipping];
+}
+
+bool _PresentApplePayAuthorization() {
+    return [Cart presentAuthorizationController];
+}
