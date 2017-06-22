765bd1874b4180949f40c8dcd1e8aca994544d5d
Stephan Leroux <stephan.leroux@shopify.com>
2017-06-22 11:20:56 -0400

    Changes test script to use Unity's Mono so the user doesn't have to install an older version on their system

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,10 @@
 which mcs &> /dev/null || die "Mono is not installed"
 which nunit-console &> /dev/null || die "nunit-console is not installed"
 
+UNITY_MONO_DIR="/Applications/Unity/MonoDevelop.app/Contents/Frameworks/Mono.framework/Versions/Current"
+
 # we're defining SHOPIFY_TEST here so that source can check whether we're running assertions/tests
-mcs -debug -define:SHOPIFY_TEST -define:SHOPIFY_MONO_UNIT_TEST -recurse:'Assets/Shopify/*.cs' -recurse:'Assets/Editor/*.cs' -reference:nunit.framework.dll -target:library -out:test.dll
+"$UNITY_MONO_DIR/bin/mcs" -debug -define:SHOPIFY_TEST -define:SHOPIFY_MONO_UNIT_TEST -recurse:'Assets/Shopify/*.cs' -recurse:'Assets/Editor/*.cs' -reference:nunit.framework.dll -target:library -out:test.dll
 
 if [ $? = 0 ] ; then
     nunit-console test.dll
