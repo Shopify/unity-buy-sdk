b3f6d6395ca9b111a9935dbaef0993b904a43ad0
Jeffrey Deng <jeffreydeng@live.com>
2017-06-22 23:46:18 -0400

    [Feature] iOS Apple Dispatcher Tests (#103)
    * Added XCTestCase for ApplePay Dispatcher
    
    * Added serializables and enums
    
    * Added ApplePay Receiver and response

---
+++ Assets/UnityTestTools/NativeTesting/Native/MessageCenterTestsHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace Shopify.Tests {
+    using UnityEngine;
+    using Shopify.Unity.SDK;
+
+    public class MessageCenterTestsHelper : MonoBehaviour {
+        void RepeatMessage(string serializedMessage) {
+            var message =  NativeMessageTester.CreateFromJSON(serializedMessage);
+            message.Respond(message.Content);
+        }
+
+        void GetLastMessage(string serializedMessage) {
+            var lastMessage = NativeMessageTester.LastMessage;
+            var message = NativeMessageTester.CreateFromJSON(serializedMessage);
+            message.Respond(lastMessage.Content);
+        }
+    }
+}
