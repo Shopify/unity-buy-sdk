28cba12cd7ba69d1fb4706c31710495252c137ec
Stephan Leroux <stephan.leroux@shopify.com>
2017-07-04 13:42:05 -0400

    Fixes issues after renaming the ShopifyError fields

--- Assets/Editor/TestShopify.cs
+++ Assets/Editor/TestShopify.cs
@@ -191,7 +191,7 @@ namespace Shopify.Tests
             ShopifyBuy.Client().products(callback: (p, error) => {
                 Assert.IsNull(p);
                 Assert.IsNotNull(error);
-                Assert.AreEqual("[\"GraphQL error from mock loader\"]", error.description);
+                Assert.AreEqual("[\"GraphQL error from mock loader\"]", error.Description);
             }, first: 250, after: "666");
         }
 
@@ -203,7 +203,7 @@ namespace Shopify.Tests
             ShopifyBuy.Client().products(callback: (p, error) => {
                 Assert.IsNull(p);
                 Assert.IsNotNull(error);
-                Assert.AreEqual("404 from mock loader", error.description);
+                Assert.AreEqual("404 from mock loader", error.Description);
             }, first: 250, after: "404");
         }
 
