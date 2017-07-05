559a20b66964b830af020c97fb823f531a414840
mikkoh <mikko.haapoja@shopify.com>
2017-07-05 17:58:07 -0400

    Added quotes to paths

--- scripts/native_tests.sh
+++ scripts/native_tests.sh
@@ -2,8 +2,8 @@
 
 # will delete all files needed for testing native extenstions
 delete_native_tests() {
-    rm -rf $PROJECT_ROOT/Assets/Plugins/iOS/Shopify/BuyTests/
-    rm -f $PROJECT_ROOT/Assets/Plugins/iOS/Shopify/Unity-iPhone-Tests-Bridging-Header.h
+    rm -rf "$PROJECT_ROOT/Assets/Plugins/iOS/Shopify/BuyTests/"
+    rm -f "$PROJECT_ROOT/Assets/Plugins/iOS/Shopify/Unity-iPhone-Tests-Bridging-Header.h"
 }
 
 # will restore all delete files needed for testing native extenstions
