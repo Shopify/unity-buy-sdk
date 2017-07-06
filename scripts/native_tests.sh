5a9c249666973fa8cae1d3f7731e9ae4d9515ee2
Mikko Haapoja <mikkoh@users.noreply.github.com>
2017-07-06 09:34:41 -0400

    [FIX] Publish Mobile Tests (#151)
    * Sets up and tears down files required for testing native extensions
    
    * Switched to using git checkout
    
    * Added quotes to paths
    
    * Reverted .gitignore to be unchanged

---
+++ scripts/native_tests.sh
@@ -0,0 +1,12 @@
+. $(dirname $0)/common.sh
+
+# will delete all files needed for testing native extenstions
+delete_native_tests() {
+    rm -rf "$PROJECT_ROOT/Assets/Plugins/iOS/Shopify/BuyTests/"
+    rm -f "$PROJECT_ROOT/Assets/Plugins/iOS/Shopify/Unity-iPhone-Tests-Bridging-Header.h"
+}
+
+# will restore all delete files needed for testing native extenstions
+restore_native_tests() {
+    git checkout $PROJECT_ROOT/Assets/Plugins/
+}
