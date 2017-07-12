1b559d39c0b9e08e880b1b148dc2c720cd92fb64
Stephan Leroux <stephan.leroux@shopify.com>
2017-07-12 14:06:11 -0400

    [Fix] Splits up Tester behaviour into Unity/iOS friendly parts to avoid editor issues (#167)
    * Splits up Tester behaviour into Unity/iOS friendly parts to avoid editor issues

--- Assets/UnityTestTools/NativeTesting/iOS/ApplePayEventTester.cs
+++ Assets/UnityTestTools/NativeTesting/iOS/ApplePayEventTester.cs
@@ -1,12 +1,16 @@
-#if UNITY_IOS
 namespace Shopify.Tests.iOS {
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+
+    #if UNITY_IOS
     using Shopify.Unity.SDK.iOS;
+    #endif
 
-    public class ApplePayEventTester : MonoBehaviour, IApplePayEventReceiver {
+    public partial class ApplePayEventTester : MonoBehaviour {}
 
+    #if UNITY_IOS
+    public partial class ApplePayEventTester : IApplePayEventReceiver {
         public void UpdateSummaryItemsForShippingIdentifier(string serializedMessage) {
             var message = NativeMessageTester.CreateFromJSON(serializedMessage);
             var response = new ApplePayEventResponse(ApplePayAuthorizationStatus.Success, GetExpectedSummaryItems());
@@ -42,5 +46,5 @@ namespace Shopify.Tests.iOS {
             return shippingMethods;
         }
     }
+    #endif
 }
-#endif
