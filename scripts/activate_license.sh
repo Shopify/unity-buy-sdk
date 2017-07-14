e47f77369e9441571a81009f199993b09f43683a
Jeffrey Deng <jeffreydeng@live.com>
2017-07-14 14:03:38 -0400

    Cleanup activation, and correct if statement

--- scripts/activate_license.sh
+++ scripts/activate_license.sh
@@ -3,7 +3,7 @@
 . $(dirname $0)/common.sh
 
 UNITY_LICENSE_LOG_PATH="$PROJECT_ROOT"/license.log
-UNITY_LICENSE_PATH= "/Library/Application\ Support/Unity/"
+UNITY_LICENSE_PATH='/Library/Application Support/Unity/'
 
 which "$UNITY_PATH" &> /dev/null || die "Unity does not exist at $UNITY_PATH"
 
@@ -18,15 +18,13 @@ if [[ -n $UNITY_USERNAME && -n $UNITY_PASSWORD && -n $UNITY_SERIAL  ]] ; then
         -username "$UNITY_USERNAME" \
         -password "$UNITY_PASSWORD" \
         -logFile "$UNITY_LICENSE_LOG_PATH"
-
-    # Here https://docs.unity3d.com/Manual/CommandLineArguments.html
-    # it states that activating the license might take a bit of time :(
-    sleep 5
 else
     echo "Unity building with default license"
 fi
 
-if [[ $? = 0 && $(ls -A $UNITY_LICENSE_PATH) ]] ; then
+LICENSE_EXIST=$(ls -A "$UNITY_LICENSE_PATH")
+
+if [[ $? = 0 && $LICENSE_EXIST ]] ; then
     echo "Acquired license"
     exit 0
 else
