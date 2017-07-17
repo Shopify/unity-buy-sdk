828f5827198c8fdd431792d64c7ddfc7dd3627d1
Jeffrey Deng <jeffreydeng@live.com>
2017-07-17 15:36:33 -0400

    Refactored scripts to fit shellcheck standards

--- scripts/activate_license.sh
+++ scripts/activate_license.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. $(dirname $0)/common.sh
+. "$(dirname $0)/common.sh"
 
 UNITY_LICENSE_LOG_PATH="$PROJECT_ROOT"/license.log
 UNITY_LICENSE_PATH='/Library/Application Support/Unity/'
@@ -9,7 +9,7 @@ which "$UNITY_PATH" &> /dev/null || die "Unity does not exist at $UNITY_PATH"
 
 # Activate license
 if [[ -n $UNITY_USERNAME && -n $UNITY_PASSWORD && -n $UNITY_SERIAL  ]] ; then
-    echo "Unity building with $UNITY_USERNAME license"
+    printf 'Unity acquiring %s license ...\n' "$UNITY_USERNAME"
 
     "$UNITY_PATH" \
         -quit \
@@ -18,18 +18,19 @@ if [[ -n $UNITY_USERNAME && -n $UNITY_PASSWORD && -n $UNITY_SERIAL  ]] ; then
         -username "$UNITY_USERNAME" \
         -password "$UNITY_PASSWORD" \
         -logFile "$UNITY_LICENSE_LOG_PATH"
+
+    ACTIVATE_SUCCESS=$?
+    LICENSE_EXIST=$(ls -A "$UNITY_LICENSE_PATH")
+
+    if [[ $ACTIVATE_SUCCESS = 0 && $LICENSE_EXIST ]] ; then
+        printf 'Acquired license\n'
+    else
+        printf '########## Failed to acquire license ##########\n'
+        cat "$UNITY_LICENSE_LOG_PATH"
+        exit 1
+    fi
 else
-    echo "Unity building with default license"
+    printf 'Unity building with default license'
 fi
 
-LICENSE_EXIST=$(ls -A "$UNITY_LICENSE_PATH")
-
-if [[ $? = 0 && $LICENSE_EXIST ]] ; then
-    echo "Acquired license"
-    exit 0
-else
-    echo "Failed to acquire license"
-    echo "------------------\n\n"
-    cat "$UNITY_LICENSE_LOG_PATH"
-    exit 1
-fi
+exit 0
