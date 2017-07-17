318fa39a40a93186f1aa40d5392bb08c3b120033
Jeffrey Deng <jeffreydeng@live.com>
2017-07-17 16:17:16 -0400

    [Fix] Cleanup CI activation (#187)
    * Cleanup activation, and correct if statement
    
    * Refactored scripts to fit shellcheck standards

--- scripts/activate_license.sh
+++ scripts/activate_license.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-. $(dirname $0)/common.sh
+. "$(dirname $0)/common.sh"
 
 UNITY_LICENSE_LOG_PATH="$PROJECT_ROOT"/license.log
-UNITY_LICENSE_PATH= "/Library/Application\ Support/Unity/"
+UNITY_LICENSE_PATH='/Library/Application Support/Unity/'
 
 which "$UNITY_PATH" &> /dev/null || die "Unity does not exist at $UNITY_PATH"
 
 # Activate license
 if [[ -n $UNITY_USERNAME && -n $UNITY_PASSWORD && -n $UNITY_SERIAL  ]] ; then
-    echo "Unity building with $UNITY_USERNAME license"
+    printf 'Unity acquiring %s license ...\n' "$UNITY_USERNAME"
 
     "$UNITY_PATH" \
         -quit \
@@ -19,19 +19,18 @@ if [[ -n $UNITY_USERNAME && -n $UNITY_PASSWORD && -n $UNITY_SERIAL  ]] ; then
         -password "$UNITY_PASSWORD" \
         -logFile "$UNITY_LICENSE_LOG_PATH"
 
-    # Here https://docs.unity3d.com/Manual/CommandLineArguments.html
-    # it states that activating the license might take a bit of time :(
-    sleep 5
+    ACTIVATE_SUCCESS=$?
+    LICENSE_EXIST=$(ls -A "$UNITY_LICENSE_PATH")
+
+    if [[ $ACTIVATE_SUCCESS = 0 && $LICENSE_EXIST ]] ; then
+        printf 'Acquired license\n'
+    else
+        printf '########## Failed to acquire license ##########\n'
+        cat "$UNITY_LICENSE_LOG_PATH"
+        exit 1
+    fi
 else
-    echo "Unity building with default license"
+    printf 'Unity building with default license'
 fi
 
-if [[ $? = 0 && $(ls -A $UNITY_LICENSE_PATH) ]] ; then
-    echo "Acquired license"
-    exit 0
-else
-    echo "Failed to acquire license"
-    echo "------------------\n\n"
-    cat "$UNITY_LICENSE_LOG_PATH"
-    exit 1
-fi
+exit 0
