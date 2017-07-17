0ecb69f2845e78ace6841e79dbff086bb71cc8de
Jeffrey Deng <jeffreydeng@live.com>
2017-07-17 16:17:16 -0400

    [Fix] Cleanup CI activation (#187)
    * Cleanup activation, and correct if statement
    
    * Refactored scripts to fit shellcheck standards

--- scripts/deactivate_license.sh
+++ scripts/deactivate_license.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. $(dirname $0)/common.sh
+. "$(dirname $0)/common.sh"
 
 UNITY_LICENSE_LOG_PATH="$PROJECT_ROOT"/deactivate_license.log
 
@@ -8,7 +8,7 @@ which "$UNITY_PATH" &> /dev/null || die "Unity does not exist at $UNITY_PATH"
 
 # Deactivate license
 if [[ -n "$UNITY_USERNAME" && -n "$UNITY_PASSWORD" && -n "$UNITY_SERIAL"  ]] ; then
-    echo "Deactivate license"
+    printf 'Deactivating license ... \n'
 
     "$UNITY_PATH" \
         -quit \
@@ -16,12 +16,12 @@ if [[ -n "$UNITY_USERNAME" && -n "$UNITY_PASSWORD" && -n "$UNITY_SERIAL"  ]] ; t
         -returnlicense \
         -logFile "$UNITY_LICENSE_LOG_PATH"
 
-    if [ $? = 0 ] ; then
-        echo "Deactivated license"
-        exit 0
+    DEACTIVATE_SUCCESS=$?
+
+    if [[ $DEACTIVATE_SUCCESS = 0 ]] ; then
+        printf 'Deactivated license\n'
     else
-        echo "Failed to deactivate license"
-        echo "------------------\n\n"
+        printf '########## Failed to deactivate license ##########\n'
         cat "$UNITY_LICENSE_LOG_PATH"
         exit 1
     fi
