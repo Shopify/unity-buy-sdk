5a0c783a070375a4668ea73c0fc2551175d5c9de
Sander Lijbrink <sander.lijbrink@shopify.com>
2017-07-18 16:20:58 -0400

    Fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ which ruby &> /dev/null || die "Ruby is not installed"
 which bundle &> /dev/null || die "Bundler is not installed"
 
 start "generating GraphQL client"
-$SCRIPTS_ROOT/generate.sh || die "Could not generate Ruby client"
+$(dirname $0)/generate.sh || die "Could not generate Ruby client"
 end "generating GraphQL client"
