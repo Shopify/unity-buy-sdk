8a40f0e73c7b7e68bb3491e372ba0532d4778988
Sander Lijbrink <sander.lijbrink@shopify.com>
2017-07-18 14:29:08 -0400

    CI on Buildkite

---
+++ scripts/ci.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eo pipefail
+
+if [ "$BUILDKITE" != "true" ] ; then
+  echo "This should run on Buildkite!"
+  exit 1
+fi
+
+function deactivate_license {
+  echo "--- Deactivate Unity license"
+  ./scripts/deactivate_license.sh
+}
+
+echo "--- Dependencies"
+source ./scripts/prepare_ci.sh
+
+echo "--- Build"
+./scripts/build.sh
+
+echo "--- Activate Unity license"
+./scripts/activate_license.sh
+
+trap deactivate_license SIGHUP SIGINT SIGTERM EXIT
+
+echo "+++ Test Unity"
+./scripts/test_unity.sh
+
+echo "+++ Test iOS"
+./scripts/test_iOS.sh
---
+++ scripts/prepare_ci.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Sourced on CI
+
+echo "--- Exporting environment variables..."
+export PROJECT_ROOT=$PWD/unity-buy-sdk
+export UNITY_DOWNLOAD_DIR=$PROJECT_ROOT/unity
+export UNITY_PKG_LOCATION=$UNITY_DOWNLOAD_DIR/Unity.pkg
+export UNITY_PKG_URL=http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity.pkg
+export IOS_PKG_LOCATION=$UNITY_DOWNLOAD_DIR/Unity-iOS.pkg
+export IOS_PKG_UR=http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-5.4.2f2.pkg
+export UNITY_CIRCLE_XML_DIR=$CIRCLE_TEST_REPORTS/Unity
+export UNITY_CIRCLE_XML_OUT_PATH=$UNITY_CIRCLE_XML_DIR/junit.xml
+
+echo "--- Booting simulator..."
+xcrun instruments -w "iPhone 6 (10.3.1) [" || true
+
+echo "--- Installing Unity"
+./scripts/install_unity.sh
