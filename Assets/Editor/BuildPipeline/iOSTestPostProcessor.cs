a428273413e2ccfd55840e69c6e2a58e5c32fedf
Stephan Leroux <stephan.leroux@shopify.com>
2017-07-19 18:25:04 -0400

    [Feature] Adds iOS Post Processor for Build & Run from Unity (#197)
    * Adds Editor/BuildPipeline to SDK for build settings post processor and refactored test generation
    
    * Fixes nested directory creation from globbed list
    
    * Fixes test_iOS.sh executeMethod namespace

---
+++ Assets/Editor/BuildPipeline/iOSTestPostProcessor.cs
@@ -0,0 +1,45 @@
+#if UNITY_EDITOR
+namespace Shopify.Unity.Editor.BuildPipeline {
+    using UnityEngine;
+    using UnityEditor;
+    using UnityEditor.iOS.Xcode;
+    using System.IO;
+    using System;
+
+    public class iOSTestPostProcessor {
+
+        /// <summary>
+        /// Perform post processing on the build to only run Shopify Tests
+        /// </summary>
+        public static void ProcessForTests(string buildPath) {
+            var project = new ExtendedPBXProject(buildPath);
+            SetBuildProperties(project);
+            SetCorrectTestsTarget(project);
+            project.RemoveFileFromBuild(project.TestTargetGuid, project.FindFileGuidByProjectPath("Unity-iPhone Tests/Unity_iPhone_Tests.m"));
+            iOSPostProcessor.SetSwiftInterfaceHeader(project);
+            project.Save();
+        }
+
+        /// Sets the correct build properties to run
+        private static void SetBuildProperties(ExtendedPBXProject project) {
+            iOSPostProcessor.SetBuildProperties(project);
+            project.SetBuildProperty(project.TestTargetGuid, ExtendedPBXProject.SwiftBridgingHeaderKey, "Libraries/Plugins/iOS/Shopify/Unity-iPhone-Tests-Bridging-Header.h");
+            project.SetBuildProperty(project.TestTargetGuid, ExtendedPBXProject.RunpathSearchKey, "@loader_path/Frameworks");
+            project.SetBuildProperty(project.TestTargetGuid, ExtendedPBXProject.ProjectModuleNameKey, "$(PRODUCT_NAME:c99extidentifier)Tests");
+            project.SetBuildPropertyForConfig(project.GetDebugConfig(project.UnityTargetGuid), ExtendedPBXProject.EnableTestabilityKey, "YES");
+        }
+
+        /// Sets the correct target for Shopify Tests
+        private static void SetCorrectTestsTarget(ExtendedPBXProject project) {
+            string testPath = Path.Combine(project.BuildPath, "Libraries/Plugins/iOS/Shopify/BuyTests/");
+            var testDirectory = new DirectoryInfo(testPath);
+
+            try {
+                project.SetFilesInDirectoryToTestTarget(testDirectory);
+            } catch (Exception e) {
+                Debug.Log(e.Message);
+            }
+        }
+    }
+}
+#endif
