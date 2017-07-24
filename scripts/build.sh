8132f64179866e96c33944664ab1bd4a0b3724e4
Stephan Leroux <stephan.leroux@shopify.com>
2017-07-24 16:32:54 -0400

    Added android building to build.sh script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,3 +6,8 @@ which bundle &> /dev/null || die "Bundler is not installed"
 start "generating GraphQL client"
 $SCRIPTS_ROOT/generate.sh || die "Could not generate Ruby client"
 end "generating GraphQL client"
+
+
+start "generating Android .AAR plugin"
+$SCRIPTS_ROOT/build_android.sh &> /dev/null || die "Could not generate Android .AAR"
+end "generating Android .AAR plugin"
