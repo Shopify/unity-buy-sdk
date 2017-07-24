c6e5547dedb389ccfb167884ce28ff3a34ade636
Stephan Leroux <stephan.leroux@shopify.com>
2017-07-24 13:19:41 -0400

    Added unit test script and moved back to using lib gradle tasks instead of root

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -5,4 +5,4 @@ ANDROID_PLUGIN_ROOT="$PROJECT_ROOT/unity_buy_android"
 start "generating Android Plugin"
 
 # Run Unity assembly task
-"$ANDROID_PLUGIN_ROOT"/gradlew assembleUnity -p "$ANDROID_PLUGIN_ROOT"
+"$ANDROID_PLUGIN_ROOT"/gradlew unity_buy_plugin:assembleUnity -p "$ANDROID_PLUGIN_ROOT" || die "Failed to generate Android AAR plugin"
