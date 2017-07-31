552feb38636f66b70205f11f81dfd4cc1f940892
Stephan Leroux <stephan.leroux@shopify.com>
2017-07-31 09:53:13 -0400

    [Quality] Adds checkstyle task to Android project (#223)

---
+++ scripts/test_android.sh
@@ -0,0 +1,5 @@
+. $(dirname $0)/common.sh
+
+ANDROID_PLUGIN_ROOT="$PROJECT_ROOT/unity_buy_android"
+
+"$ANDROID_PLUGIN_ROOT"/gradlew unity_buy_plugin:check -p "$ANDROID_PLUGIN_ROOT" || die "Failed to check Android plugin"
