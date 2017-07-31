912c88d185f25982feaa7db2e765af5712009d0e
Stephan Leroux <stephan.leroux@shopify.com>
2017-07-31 10:39:16 -0400

    [Cleanup] Android folder renaming (#225)
    * Renames unity_buy_android to just android in the root folder. 
    * Renames android module to buy_plugin to help disambiguate between Unity
    
    * Fixes scripts to use new Android module
    
    * Moves checkstyle stuff into new folder

--- scripts/test_android.sh
+++ scripts/test_android.sh
@@ -1,5 +1,5 @@
 . $(dirname $0)/common.sh
 
-ANDROID_PLUGIN_ROOT="$PROJECT_ROOT/unity_buy_android"
+ANDROID_PLUGIN_ROOT="$PROJECT_ROOT/android"
 
-"$ANDROID_PLUGIN_ROOT"/gradlew unity_buy_plugin:check -p "$ANDROID_PLUGIN_ROOT" || die "Failed to check Android plugin"
+"$ANDROID_PLUGIN_ROOT"/gradlew shopify_buy_plugin:check -p "$ANDROID_PLUGIN_ROOT" || die "Failed to check Android plugin"
