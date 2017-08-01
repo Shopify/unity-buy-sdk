9c2b7f31d1b74db13a64c49fae3680de045e8549
Jeffrey Deng <jeffreydeng@live.com>
2017-08-01 16:13:00 -0400

    [Feature] Android unity message (#240)
    * Added UnityMessage on Android

---
+++ android/shopify_buy_plugin/src/test/java/com/shopify/unity/buy/UnityMessageTest.java
@@ -0,0 +1,38 @@
+package com.shopify.unity.buy;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+public class UnityMessageTest {
+
+    @Test
+    public void testSerialize() throws JSONException {
+
+        final String identifierKey = UnityMessage.IDENTIFIER_KEY;
+        final String contentKey = UnityMessage.CONTENT_KEY;
+        final String expectedContent = "expectedContent";
+
+        UnityMessage messageA = new UnityMessage(expectedContent);
+        String messageAJsonString = messageA.toJsonString();
+        JSONObject jsonA = new JSONObject(messageAJsonString);
+
+        UnityMessage messageB = new UnityMessage(expectedContent);
+        String messageBJsonString = messageB.toJsonString();
+        JSONObject jsonB = new JSONObject(messageBJsonString);
+
+        assertTrue(jsonA.has(identifierKey));
+        assertTrue(jsonA.has(contentKey));
+        assertEquals(jsonA.get(contentKey), expectedContent);
+
+        String identifierA = jsonA.getString(identifierKey);
+        String identifierB = jsonB.getString(identifierKey);
+
+        assertFalse(identifierA.equals(identifierB));
+        assertEquals(jsonA.get(contentKey), jsonB.get(contentKey));
+    }
+}
