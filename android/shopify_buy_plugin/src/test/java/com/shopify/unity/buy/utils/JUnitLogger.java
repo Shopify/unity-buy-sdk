7b5e28d316f52c52ffad1fc62f00d17c3c1cee3c
Stephan Leroux <stephan.leroux@shopify.com>
2017-08-02 14:38:17 -0400

    Removed no longer needed test files since everything just happens using Moshi

--- android/shopify_buy_plugin/src/test/java/com/shopify/unity/buy/utils/JUnitLogger.java
+++
@@ -1,8 +0,0 @@
-package com.shopify.unity.buy.utils;
-
-public class JUnitLogger implements ILogger {
-    @Override
-    public void error(String tag, String message) {
-        System.out.println(tag + ":" + message);
-    }
-}
