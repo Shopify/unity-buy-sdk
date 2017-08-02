0c351edb8fe691dd8fe5455c7ff7f1a025dad9fa
Jeffrey Deng <jeffreydeng@live.com>
2017-08-02 17:35:20 -0400

    [Feature] Added Android Web Intent Fragment (#244)
    * Added Web Intent Fragment

---
+++ android/shopify_buy_plugin/src/main/java/com/shopify/unity/buy/WebIntentFragment.java
@@ -0,0 +1,60 @@
+package com.shopify.unity.buy;
+
+import android.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.customtabs.CustomTabsIntent;
+
+public final class WebIntentFragment extends Fragment {
+
+    private static final String ARGS_URL = "url";
+    private boolean onPauseIssued = false;
+    private WebIntentListener listener;
+
+    public static WebIntentFragment newInstance(String url) {
+        Bundle args = new Bundle();
+        args.putString(ARGS_URL, url);
+
+        WebIntentFragment fragment = new WebIntentFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String url = getArguments().getString(ARGS_URL);
+        if (url != null) {
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            CustomTabsIntent customTabsIntent = builder.build();
+            customTabsIntent.launchUrl(getActivity().getApplicationContext(), Uri.parse(url));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        onPauseIssued = true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (onPauseIssued) {
+            notifyListenerOnClose();
+        }
+        onPauseIssued = false;
+    }
+
+    public void setListener(WebIntentListener listener) {
+        this.listener = listener;
+    }
+
+    private void notifyListenerOnClose() {
+        if (listener != null) {
+            listener.onWebIntentClose();
+        }
+    }
+}
+
