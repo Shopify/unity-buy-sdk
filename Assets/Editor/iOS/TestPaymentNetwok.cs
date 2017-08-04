2f729cdf7d00ef8f33908c526640a1a29ae358b5
Jeffrey Deng <jeffreydeng@live.com>
2017-08-04 17:35:22 -0400

    Added unity tests for payment networks

---
+++ Assets/Editor/iOS/TestPaymentNetwok.cs
@@ -0,0 +1,49 @@
+#if UNITY_IOS
+namespace Shopify.Tests {
+    using System;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using Shopify.Unity;
+    using UnityEngine;
+    using Shopify.Unity.SDK.iOS;
+    using Shopify.Unity.MiniJSON;
+
+    [TestFixture]
+    public class TestPaymentNetwork {
+
+        [Test]
+        public void TestFromCardBrandsNonNull() {
+
+            var cardBrands = new List<CardBrand>();
+            cardBrands.Add(CardBrand.AMERICAN_EXPRESS);
+            cardBrands.Add(CardBrand.DISCOVER);
+            cardBrands.Add(CardBrand.JCB);
+            cardBrands.Add(CardBrand.MASTERCARD);
+            cardBrands.Add(CardBrand.VISA);
+
+            var paymentNetworks = PaymentNetwork.NetworksFromCardBrands(cardBrands);
+            Assert.AreEqual(cardBrands.Count, paymentNetworks.Count, "Not all CardBrands were converted to PaymentNetworks");
+
+            for (int i = 0; i < cardBrands.Count; i ++) {
+
+                var expectedPaymentNetwork = PaymentNetwork.FromCardBrand(cardBrands[i]);
+                var actualPaymentNetwork = paymentNetworks[i];
+
+                Assert.AreEqual(
+                    expectedPaymentNetwork.ToString(),
+                    actualPaymentNetwork.ToString(),
+                    "Results from PaymentNetwork.FromCardBrands and PaymentNetwork.FromCardBrand differ for a CardBrand");
+            }
+        }
+
+        [Test]
+        public void TestFromCardBrandsNull() {
+            var cardBrands = new List<CardBrand>();
+            cardBrands.Add(CardBrand.UNKNOWN);
+
+            var paymentNetworks = PaymentNetwork.NetworksFromCardBrands(cardBrands);
+            Assert.AreEqual(0, paymentNetworks.Count, "Expected PaymentNetwork from an unsupported CardBrand to have returned null");
+        }
+    }
+}
+#endif
