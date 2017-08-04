0ce4740cf9950fae207e54c33038928861e42078
Jeffrey Deng <jeffreydeng@live.com>
2017-08-04 13:44:34 -0400

    Refactor license scripts to fix lint issues and fixes #190 (#243)

--- scripts/activate_license.sh
+++ scripts/activate_license.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-. "$(dirname $0)/common.sh"
+. "$(dirname "$0")"/common.sh
+. "$(dirname "$0")"/has_license_credentials.sh
 
 UNITY_LICENSE_LOG_PATH="$PROJECT_ROOT"/license.log
 UNITY_LICENSE_PATH='/Library/Application Support/Unity/'
 
 which "$UNITY_PATH" &> /dev/null || die "Unity does not exist at $UNITY_PATH"
 
-# Activate license
-if [[ -n $UNITY_USERNAME && -n $UNITY_PASSWORD && -n $UNITY_SERIAL  ]] ; then
-    printf 'Unity acquiring %s license ...\n' "$UNITY_USERNAME"
+has_license_credentials || {
+    printf "Unity building with default license\n"
+    exit 0
+}
 
-    "$UNITY_PATH" \
-        -quit \
-        -batchmode \
-        -serial "$UNITY_SERIAL" \
-        -username "$UNITY_USERNAME" \
-        -password "$UNITY_PASSWORD" \
-        -logFile "$UNITY_LICENSE_LOG_PATH"
+printf 'Unity acquiring %s license ...\n' "$UNITY_USERNAME"
 
-    ACTIVATE_SUCCESS=$?
-    LICENSE_EXIST=$(ls -A "$UNITY_LICENSE_PATH")
+"$UNITY_PATH" \
+    -quit \
+    -batchmode \
+    -serial "$UNITY_SERIAL" \
+    -username "$UNITY_USERNAME" \
+    -password "$UNITY_PASSWORD" \
+    -logFile "$UNITY_LICENSE_LOG_PATH"
 
-    if [[ $ACTIVATE_SUCCESS = 0 && $LICENSE_EXIST ]] ; then
-        printf 'Acquired license\n'
-    else
-        printf '########## Failed to acquire license ##########\n'
-        cat "$UNITY_LICENSE_LOG_PATH"
-        exit 1
-    fi
+ACTIVATE_SUCCESS=$?
+LICENSE_EXIST=$(ls -A "$UNITY_LICENSE_PATH")
+
+if [[ $ACTIVATE_SUCCESS = 0 && $LICENSE_EXIST ]] ; then
+    printf 'Acquired license\n'
+    exit 0
 else
-    printf 'Unity building with default license'
+    printf '########## Failed to acquire license ##########\n'
+    cat "$UNITY_LICENSE_LOG_PATH"
+    exit 1
 fi
-
-exit 0
