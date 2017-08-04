bed23018e13cd422eccecd30df03f68d6d4bce7d
Jeffrey Deng <jeffreydeng@live.com>
2017-08-04 13:44:34 -0400

    Refactor license scripts to fix lint issues and fixes #190 (#243)

--- scripts/deactivate_license.sh
+++ scripts/deactivate_license.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-. "$(dirname $0)/common.sh"
+. "$(dirname "$0")"/common.sh
+. "$(dirname "$0")"/has_license_credentials.sh
 
 UNITY_LICENSE_LOG_PATH="$PROJECT_ROOT"/deactivate_license.log
 
 which "$UNITY_PATH" &> /dev/null || die "Unity does not exist at $UNITY_PATH"
 
-# Deactivate license
-if [[ -n "$UNITY_USERNAME" && -n "$UNITY_PASSWORD" && -n "$UNITY_SERIAL"  ]] ; then
-    printf 'Deactivating license ... \n'
+has_license_credentials || {
+    exit 0
+}
 
-    "$UNITY_PATH" \
-        -quit \
-        -batchmode \
-        -returnlicense \
-        -logFile "$UNITY_LICENSE_LOG_PATH"
+printf 'Deactivating license ... \n'
 
-    DEACTIVATE_SUCCESS=$?
+"$UNITY_PATH" \
+    -quit \
+    -batchmode \
+    -returnlicense \
+    -logFile "$UNITY_LICENSE_LOG_PATH"
 
-    if [[ $DEACTIVATE_SUCCESS = 0 ]] ; then
-        printf 'Deactivated license\n'
-    else
-        printf '########## Failed to deactivate license ##########\n'
-        cat "$UNITY_LICENSE_LOG_PATH"
-        exit 1
-    fi
+DEACTIVATE_SUCCESS=$?
+
+if [[ $DEACTIVATE_SUCCESS = 0 ]] ; then
+    printf 'Deactivated license\n'
+    exit 0
+else
+    printf '########## Failed to deactivate license ##########\n'
+    cat "$UNITY_LICENSE_LOG_PATH"
+    exit 1
 fi
-
-exit 0
