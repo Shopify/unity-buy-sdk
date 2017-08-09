c9c228e9d0dc6b7bfba507b17a8e810a6f76334e
Stephan Leroux <stephan.leroux@shopify.com>
2017-08-09 10:57:29 -0400

    [Android Pay] Launches wallet chooser UI when checking out using Native Pay from Unity (#250)
    * Fills out UnityAndroidPayFragment with additional information and connects to Google APIs and launches Android Pay Masked Request
    
    * Added bundle validation when building UnityAndroidPayFragment
    
    * Fixed minor code review issues with variable ordering and spacing
    
    * [Android Pay] Adds plumbing for sending and receiving messages to and from Unity (#251)
    
    * Add session callbacks and added checkout state to fragment to persist our current state of checking out
    
    * Updated PR comments

--- android/shopify_buy_plugin/src/main/java/com/shopify/unity/buy/AndroidPayCheckoutSession.java
+++ android/shopify_buy_plugin/src/main/java/com/shopify/unity/buy/AndroidPayCheckoutSession.java
@@ -1,6 +1,11 @@
 package com.shopify.unity.buy;
 
+import android.app.Fragment;
+
+import com.google.android.gms.wallet.WalletConstants;
 import com.shopify.buy3.pay.PayCart;
+import com.shopify.buy3.pay.PayHelper;
+import com.shopify.unity.buy.models.MailingAddressInput;
 import com.shopify.unity.buy.models.PricingLineItems;
 import com.shopify.unity.buy.utils.AndroidLogger;
 import com.shopify.unity.buy.utils.ILogger;
@@ -8,38 +13,53 @@ import com.unity3d.player.UnityPlayer;
 
 import java.io.IOException;
 
-public final class AndroidPayCheckoutSession {
+public final class AndroidPayCheckoutSession implements AndroidPaySessionCallbacks {
+    private static final String PAY_FRAGMENT_TAG = "payFragment";
+
     private ILogger logger;
+    private String unityDelegateObjectName;
 
     public AndroidPayCheckoutSession() {
         this.logger = new AndroidLogger();
     }
 
-    public AndroidPayCheckoutSession(ILogger logger) {
+    AndroidPayCheckoutSession(ILogger logger) {
         this.logger = logger;
     }
 
+    //CHECKSTYLE:OFF
     public boolean checkoutWithAndroidPay(
             String unityDelegateObjectName,
             String merchantName,
+            String publicKey,
             String pricingLineItemsString,
             String currencyCode,
             String countryCode,
-            boolean requiresShipping
+            boolean requiresShipping,
+            boolean testing
     ) {
+        //CHECKSTYLE:ON
+        if (!PayHelper.isAndroidPayEnabledInManifest(UnityPlayer.currentActivity)) {
+            // TODO: Send unsupported error to Unity
+            return false;
+        }
+
         try {
             PayCart cart = cartFromUnity(merchantName, pricingLineItemsString, currencyCode, countryCode,
                     requiresShipping);
-            addAndroidPayFragment(unityDelegateObjectName, cart, countryCode);
+
+            this.unityDelegateObjectName = unityDelegateObjectName;
+
+            addPayFragment(cart, countryCode, publicKey, testing);
+
             return true;
         } catch (IOException e) {
-            logger.error("ShopifyBuyPlugin", "Failed to parse summary items from Unity");
-            e.printStackTrace();
+            logger.error("ShopifyBuyPlugin", "Failed to parse summary items from Unity!");
             return false;
         }
     }
 
-    public PayCart cartFromUnity(
+    PayCart cartFromUnity(
         String merchantName,
         String pricingLineItemsString,
         String currencyCode,
@@ -61,12 +81,62 @@ public final class AndroidPayCheckoutSession {
                 .build();
     }
 
-    private void addAndroidPayFragment(String unityDelegateObjectName, PayCart cart, String countryCode) {
-        UnityAndroidPayFragment payFragment =
-                UnityAndroidPayFragment.newInstance(unityDelegateObjectName, cart, countryCode);
+    private void addPayFragment(PayCart cart, String countryCode, String publicKey, boolean testing) {
+        removePayFragment();
+
+        Fragment payFragment = UnityAndroidPayFragment.builder()
+            .setPayCart(cart)
+            .setCountryCode(countryCode)
+            .setEnvironment(testing ?
+                WalletConstants.ENVIRONMENT_TEST : WalletConstants.ENVIRONMENT_PRODUCTION)
+            .setPublicKey(publicKey)
+            .setSessionCallbacks(this)
+            .build();
+
         UnityPlayer.currentActivity.getFragmentManager()
-                .beginTransaction()
-                .add(payFragment, "payFragment")
-                .commit();
+            .beginTransaction()
+            .add(payFragment, PAY_FRAGMENT_TAG)
+            .commit();
+    }
+
+    private void removePayFragment() {
+        Fragment fragment = UnityPlayer.currentActivity.getFragmentManager().findFragmentByTag(PAY_FRAGMENT_TAG);
+        if (fragment == null)  {
+            return;
+        }
+
+        UnityPlayer.currentActivity.getFragmentManager()
+            .beginTransaction()
+            .remove(fragment)
+            .commit();
+    }
+
+    public void onUpdateShippingAddress(MailingAddressInput address, MessageCenter.MessageCallbacks messageCallbacks) {
+        UnityMessage msg = UnityMessage.fromAndroid(address.toJsonString());
+        MessageCenter.UnityMessageReceiver receiver = new MessageCenter.UnityMessageReceiver(
+            unityDelegateObjectName,
+            MessageCenter.Method.ON_UPDATE_SHIPPING_ADDRESS
+        );
+        MessageCenter.sendMessageTo(msg, receiver, messageCallbacks);
+    }
+
+    public void onError(String error) {
+        removePayFragment();
+        UnityMessage msg = UnityMessage.fromAndroid(error);
+        MessageCenter.UnityMessageReceiver receiver = new MessageCenter.UnityMessageReceiver(
+            unityDelegateObjectName,
+            MessageCenter.Method.ON_ERROR
+        );
+        MessageCenter.sendMessageTo(msg, receiver);
+    }
+
+    public void onCancel() {
+        removePayFragment();
+        UnityMessage msg = UnityMessage.fromAndroid("");
+        MessageCenter.UnityMessageReceiver receiver = new MessageCenter.UnityMessageReceiver(
+            unityDelegateObjectName,
+            MessageCenter.Method.ON_CANCEL
+        );
+        MessageCenter.sendMessageTo(msg, receiver);
     }
 }
---
+++ android/shopify_buy_plugin/src/main/java/com/shopify/unity/buy/models/MailingAddressInput.java
@@ -0,0 +1,22 @@
+package com.shopify.unity.buy.models;
+
+import com.google.android.gms.identity.intents.model.UserAddress;
+import com.shopify.buy3.pay.PayAddress;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+// A wrapper around PayAddress to convert it to Json.
+public class MailingAddressInput {
+    public final PayAddress payAddress;
+
+    private static final JsonAdapter<PayAddress> jsonAdapter =
+        new Moshi.Builder().build().adapter(PayAddress.class);
+
+    public MailingAddressInput(UserAddress userAddress) {
+        payAddress = PayAddress.fromUserAddress(userAddress);
+    }
+
+    public String toJsonString() {
+        return jsonAdapter.toJson(payAddress);
+    }
+}
