08cc96c1e0d28a887aa428dec344c74c107680e2
Stephan Leroux <stephan.leroux@shopify.com>
2017-08-09 10:57:29 -0400

    [Android Pay] Launches wallet chooser UI when checking out using Native Pay from Unity (#250)
    * Fills out UnityAndroidPayFragment with additional information and connects to Google APIs and launches Android Pay Masked Request
    
    * Added bundle validation when building UnityAndroidPayFragment
    
    * Fixed minor code review issues with variable ordering and spacing
    
    * [Android Pay] Adds plumbing for sending and receiving messages to and from Unity (#251)
    
    * Add session callbacks and added checkout state to fragment to persist our current state of checking out
    
    * Updated PR comments

--- android/shopify_buy_plugin/src/main/java/com/shopify/unity/buy/UnityAndroidPayFragment.java
+++ android/shopify_buy_plugin/src/main/java/com/shopify/unity/buy/UnityAndroidPayFragment.java
@@ -1,33 +1,109 @@
 package com.shopify.unity.buy;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wallet.MaskedWallet;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
 import com.shopify.buy3.pay.PayCart;
+import com.shopify.buy3.pay.PayHelper;
+import com.shopify.unity.buy.models.MailingAddressInput;
+import com.shopify.unity.buy.utils.WalletErrorFormatter;
 
-public class UnityAndroidPayFragment extends Fragment {
-    private PayCart cart;
-    private String unityDelegateObjectName;
-    private String countryCode;
+import java.util.ArrayList;
+import java.util.List;
 
-    private static final String EXTRA_UNITY_DELEGATE_OBJECT_NAME = "unityDelegateObjectName";
+public class UnityAndroidPayFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks {
+
+    private enum CheckoutState {
+        READY,
+        REQUESTING_MASKED_WALLET,
+        RECEIVED_MASKED_WALLET,
+    }
+
     private static final String EXTRA_PAY_CART = "payCart";
     private static final String EXTRA_COUNTRY_CODE = "countryCode";
+    private static final String EXTRA_ANDROID_PAY_ENVIRONMENT = "androidPayEnvironment";
+    private static final String EXTRA_PUBLIC_KEY = "publicKey";
 
-    public static final UnityAndroidPayFragment newInstance(
-        String unityDelegateObjectName,
-        PayCart cart,
-        String countryCode
-    ) {
+    private PayCart cart;
+    private String countryCode;
+    private String publicKey;
+    private int androidPayEnvironment;
+    private GoogleApiClient googleApiClient;
+    private AndroidPaySessionCallbacks sessionCallbacks;
 
-        UnityAndroidPayFragment fragment = new UnityAndroidPayFragment();
-        Bundle bundle = new Bundle(3);
-        bundle.putString(EXTRA_UNITY_DELEGATE_OBJECT_NAME, unityDelegateObjectName);
-        bundle.putParcelable(EXTRA_PAY_CART, cart);
-        bundle.putString(EXTRA_COUNTRY_CODE, countryCode);
-        fragment.setArguments(bundle);
-        return fragment;
+    private CheckoutState currentCheckoutState;
+    private MaskedWallet maskedWallet;
+
+    static final class UnityAndroidPayFragmentBuilder {
+        private static final String[] requiredExtras = {
+            EXTRA_PAY_CART,
+            EXTRA_COUNTRY_CODE,
+            EXTRA_ANDROID_PAY_ENVIRONMENT,
+            EXTRA_PUBLIC_KEY
+        };
+
+        private Bundle bundle;
+        private AndroidPaySessionCallbacks callbacks;
+
+        UnityAndroidPayFragmentBuilder() {
+            bundle = new Bundle();
+        }
+
+        UnityAndroidPayFragmentBuilder setPayCart(PayCart cart) {
+            bundle.putParcelable(EXTRA_PAY_CART, cart);
+            return this;
+        }
+
+        UnityAndroidPayFragmentBuilder setCountryCode(String countryCode) {
+            bundle.putString(EXTRA_COUNTRY_CODE, countryCode);
+            return this;
+        }
+
+        UnityAndroidPayFragmentBuilder setEnvironment(int environment) {
+            bundle.putInt(EXTRA_ANDROID_PAY_ENVIRONMENT, environment);
+            return this;
+        }
+
+        UnityAndroidPayFragmentBuilder setPublicKey(String key) {
+            bundle.putString(EXTRA_PUBLIC_KEY, key);
+            return this;
+        }
+
+        UnityAndroidPayFragmentBuilder setSessionCallbacks(AndroidPaySessionCallbacks callbacks) {
+            this.callbacks = callbacks;
+            return this;
+        }
+
+        UnityAndroidPayFragment build() {
+            checkBundleContainsRequiredExtras();
+            UnityAndroidPayFragment fragment = new UnityAndroidPayFragment();
+            fragment.setArguments(bundle);
+            fragment.setSessionCallbacks(callbacks);
+            return fragment;
+        }
+
+        private void checkBundleContainsRequiredExtras() {
+            List<String> missingExtras = new ArrayList<>();
+            for (String extra : requiredExtras) {
+                if (bundle.get(extra) == null) {
+                    missingExtras.add(extra);
+                }
+            }
+
+            if (missingExtras.size() > 0) {
+                throw new IllegalArgumentException("Missing required bundle extras: " + missingExtras.toString());
+            }
+        }
+    }
+
+    public static UnityAndroidPayFragmentBuilder builder() {
+        return new UnityAndroidPayFragmentBuilder();
     }
 
     @Override
@@ -36,12 +112,89 @@ public class UnityAndroidPayFragment extends Fragment {
 
         Bundle bundle = getArguments();
         if (bundle != null) {
-            unityDelegateObjectName = bundle.getString(EXTRA_UNITY_DELEGATE_OBJECT_NAME);
             cart = bundle.getParcelable(EXTRA_PAY_CART);
             countryCode = bundle.getString(EXTRA_COUNTRY_CODE);
+            androidPayEnvironment = bundle.getInt(EXTRA_ANDROID_PAY_ENVIRONMENT,
+                    WalletConstants.ENVIRONMENT_TEST);
+            publicKey = bundle.getString(EXTRA_PUBLIC_KEY);
         }
 
         // Don't recreate this fragment during configuration change.
         setRetainInstance(true);
+
+        currentCheckoutState = CheckoutState.READY;
+        googleApiClient = new GoogleApiClient.Builder(this.getActivity())
+            .addApi(Wallet.API, new Wallet.WalletOptions.Builder()
+                    .setEnvironment(androidPayEnvironment)
+                    .setTheme(WalletConstants.THEME_DARK)
+                    .build())
+            .addConnectionCallbacks(this)
+            .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (currentCheckoutState == CheckoutState.READY) {
+            currentCheckoutState = CheckoutState.REQUESTING_MASKED_WALLET;
+            PayHelper.requestMaskedWallet(googleApiClient, cart, publicKey);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) { }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        PayHelper.handleWalletResponse(requestCode, resultCode, data, new PayHelper.WalletResponseHandler() {
+
+            @Override
+            public void onMaskedWallet(final MaskedWallet maskedWallet) {
+                super.onMaskedWallet(maskedWallet);
+                UnityAndroidPayFragment.this.maskedWallet = maskedWallet;
+                currentCheckoutState = CheckoutState.RECEIVED_MASKED_WALLET;
+
+                MailingAddressInput input = new MailingAddressInput(maskedWallet.getBuyerShippingAddress());
+
+                if (sessionCallbacks != null) {
+                    sessionCallbacks.onUpdateShippingAddress(input, new MessageCenter.MessageCallbacks() {
+                        @Override
+                        public void onResponse(String jsonResponse) {
+                            // TODO: Create a new pay cart with the updated shipping address and request full wallet
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onWalletError(int requestCode, int errorCode) {
+                if (sessionCallbacks != null) {
+                    sessionCallbacks.onError(WalletErrorFormatter.errorStringFromCode(errorCode));
+                }
+            }
+
+            @Override
+            public void onWalletRequestCancel(int requestCode) {
+                if (sessionCallbacks != null) {
+                    sessionCallbacks.onCancel();
+                }
+            }
+        });
+    }
+
+    public void setSessionCallbacks(AndroidPaySessionCallbacks callbacks) {
+        sessionCallbacks = callbacks;
     }
 }
