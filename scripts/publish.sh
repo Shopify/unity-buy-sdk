d71ed05d8557e0621aead9b3fe2a9c49d9c8c2b8
Jeffrey Deng <jeffreydeng@live.com>
2017-08-10 13:25:57 -0400

    Refactored publish script to fix lint issues (#263)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-. $(dirname $0)/common.sh
-. $(dirname $0)/native_tests.sh
+. "$(dirname "$0")"/common.sh
+. "$(dirname "$0")"/native_tests.sh
 
 check() {
-    if [ $? = 0 ] ; then
-        echo "Export $@ finished properly."
+    if [[ $? = 0 ]] ; then
+        printf "Export %s finished properly\n." "$@"
     else
-        echo "Export $@ failed. Exited with $?"
-        echo "------------------\n\n"
-        cat $UNITY_LOG_PATH 
+        printf "Export %s failed. Exited with %s\n" "$@" "$?"
+        printf "------------------\n\n"
+        cat "$UNITY_LOG_PATH"
 
         exit 1
     fi
@@ -19,72 +19,72 @@ check() {
 delete_native_tests
 
 # check if we need to do a major, minor, patch update
-VERSION=`cat $SCRIPTS_ROOT/version`
+VERSION=$(cat "$SCRIPTS_ROOT"/version)
 
 VERSION_ARRAY=( ${VERSION//./ } )
-PUBLISH_DESTINATION=$2
+PUBLISH_DESTINATION="$2"
 UNITY_PACKAGE=shopify-buy.unitypackage
 
-if [ $# -eq 0 ] ; then
-    echo "If you'd like to bump versions pass: major, minor, or patch. Using $VERSION for now."
-elif [ $1 = "major" ] ; then
+if [[ $# -eq 0 ]] ; then
+    printf "If you'd like to bump versions pass: major, minor, or patch. Using %s for now.\n" "$VERSION"
+elif [[ $1 = "major" ]] ; then
     ((VERSION_ARRAY[0]++))
     VERSION_ARRAY[1]=0
     VERSION_ARRAY[2]=0
-elif [ $1 = "minor" ] ; then
+elif [[ $1 = "minor" ]] ; then
     ((VERSION_ARRAY[1]++))
     VERSION_ARRAY[2]=0
-elif [ $1 = "patch" ] ; then
+elif [[ $1 = "patch" ]] ; then
     ((VERSION_ARRAY[2]++))
-elif [ $1 = "github" -o $1 = "asset-store" ] ; then
-    PUBLISH_DESTINATION=$1
+elif [[ $1 = "github" || $1 = "asset-store" ]] ; then
+    PUBLISH_DESTINATION="$1"
 else
-    echo -e "\nInvalid first parameter: \"$1\". You must pass in either a version bump param or a publish destination:\nmajor,\nminor,\npatch,\ngithub,\nasset-store"
+    printf "\nInvalid first parameter: \"%s\". You must pass in either a version bump param or a publish destination:\nmajor,\nminor,\npatch,\ngithub,\nasset-store" "$1"
     exit 1
 fi
 
-if [ "$PUBLISH_DESTINATION" != "github" ] && [ "$PUBLISH_DESTINATION" != "asset-store" ] ; then
+if [[ $PUBLISH_DESTINATION != "github" ]] && [[ $PUBLISH_DESTINATION != "asset-store" ]] ; then
     die "\nInvalid publish target.\n\nPublish target should be:\n\"asset-store\"\n\"github\"\n\nExample: scripts/publish.sh github\n"
 fi
 
 VERSION="${VERSION_ARRAY[0]}.${VERSION_ARRAY[1]}.${VERSION_ARRAY[2]}"
 
-echo $VERSION > $SCRIPTS_ROOT/version
-echo "Version used $1: $VERSION"
+echo "$VERSION" > "$SCRIPTS_ROOT"/version
+printf "Version used %s: %s\n" "$1" "$VERSION"
 
 # Run generate.sh to create source code including the potentially new version number
-$SCRIPTS_ROOT/generate.sh $PUBLISH_DESTINATION
+"$SCRIPTS_ROOT"/generate.sh "$PUBLISH_DESTINATION"
 check "generate"
 
 # Run tests just in case
-$SCRIPTS_ROOT/test.sh
+"$SCRIPTS_ROOT"/test.sh
 check "test"
 
 # Now we'll attempt to actually generate the unitypackage
-UNITY_LOG_PATH=$PROJECT_ROOT/export.log
+UNITY_LOG_PATH="$PROJECT_ROOT"/export.log
 
-which $UNITY_PATH &> /dev/null || die "Unity does not exist at $UNITY_PATH" 
+which "$UNITY_PATH" &> /dev/null || die "Unity does not exist at $UNITY_PATH"
 
-$SCRIPTS_ROOT/build_documentation.sh
+"$SCRIPTS_ROOT"/build_documentation.sh
 check "docs"
 
 # copy EXAMPLES.md to Assets/Shopify/examples.txt
-cp $PROJECT_ROOT/EXAMPLES.md $PROJECT_ROOT/Assets/Shopify/examples.txt
+cp "$PROJECT_ROOT"/EXAMPLES.md "$PROJECT_ROOT"/Assets/Shopify/examples.txt
 
-if [ "$PUBLISH_DESTINATION" = "asset-store" ] ; then
+if [[ $PUBLISH_DESTINATION = "asset-store" ]] ; then
     UNITY_PACKAGE=shopify-buy-asset-store.unitypackage
 fi
 
-echo "Exporting the unitypackage at: $UNITY_PACKAGE"
+printf "Exporting the unitypackage at: %s\n" "$UNITY_PACKAGE"
 
 # create the new unitypackage
-$UNITY_PATH \
+"$UNITY_PATH" \
     -batchmode \
     -nographics \
     -silent-crashes \
-    -logFile $UNITY_LOG_PATH \
-    -projectPath $PROJECT_ROOT \
-    -exportPackage Assets/Shopify Assets/Plugins $UNITY_PACKAGE \
+    -logFile "$UNITY_LOG_PATH" \
+    -projectPath "$PROJECT_ROOT" \
+    -exportPackage Assets/Shopify Assets/Plugins "$UNITY_PACKAGE" \
     -quit
 
 PUBLISH_SUCCESS=$?
@@ -94,13 +94,13 @@ restore_native_tests
 
 if [[ $PUBLISH_SUCCESS = 0 ]] ; then
     # clean up examples.txt
-    rm $PROJECT_ROOT/Assets/Shopify/examples.txt
+    rm "$PROJECT_ROOT"/Assets/Shopify/examples.txt
 
-    echo "Export finished"
+    printf "Export finished\n"
 else
-    echo "Export failed. Exited with $?"
-    echo "------------------\n\n"
-    cat $UNITY_LOG_PATH 
+    printf "Export failed. Exited with %s\n" "$?"
+    printf "------------------\n\n"
+    cat "$UNITY_LOG_PATH"
 
     exit 1
 fi
