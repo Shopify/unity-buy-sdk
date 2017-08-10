2c9c1762d914b203485bc1132b82c831a0a24f5f
Stephan Leroux <stephan.leroux@shopify.com>
2017-08-10 13:23:58 -0400

    Removes usage of handler/looper from MessageCenter

--- android/shopify_buy_plugin/src/main/java/com/shopify/unity/buy/MessageCenter.java
+++ android/shopify_buy_plugin/src/main/java/com/shopify/unity/buy/MessageCenter.java
@@ -1,54 +1,33 @@
 package com.shopify.unity.buy;
 
-import android.os.Handler;
-import android.os.HandlerThread;
-
 import com.unity3d.player.UnityPlayer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MessageCenter {
-    private static final HandlerThread handlerThread = new HandlerThread("UnityMessageThread");
-    private static final Handler messageHandler = new Handler(handlerThread.getLooper());
-
     private static Map<String, MessageCallbacks> callbacksInWaiting = new HashMap<>();
 
     private MessageCenter() { }
 
     // Fire and forget sending of messages.
     static void sendMessageTo(final UnityMessage msg, final UnityMessageReceiver receiver) {
-        messageHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                UnityPlayer.UnitySendMessage(receiver.unityDelegateObjectName, receiver.method.name, msg.toJsonString());
-            }
-        });
+        UnityPlayer.UnitySendMessage(receiver.unityDelegateObjectName, receiver.method.name, msg.toJsonString());
     }
 
     // Send message with callbacks to invoke when complete.
     static void sendMessageTo(final UnityMessage msg, final UnityMessageReceiver receiver, final MessageCallbacks callbacks) {
-        messageHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                callbacksInWaiting.put(msg.identifier, callbacks);
-                UnityPlayer.UnitySendMessage(receiver.unityDelegateObjectName, receiver.method.name, msg.toJsonString());
-            }
-        });
+        callbacksInWaiting.put(msg.identifier, callbacks);
+        UnityPlayer.UnitySendMessage(receiver.unityDelegateObjectName, receiver.method.name, msg.toJsonString());
     }
 
     @SuppressWarnings("unused")
     public static void onUnityResponse(final String result) {
-        messageHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                UnityMessage msg = UnityMessage.fromUnity(result);
-                MessageCallbacks callbacks = callbacksInWaiting.remove(msg.identifier);
-                if (callbacks != null) {
-                    callbacks.onResponse(msg.content);
-                }
-            }
-        });
+        UnityMessage msg = UnityMessage.fromUnity(result);
+        MessageCallbacks callbacks = callbacksInWaiting.remove(msg.identifier);
+        if (callbacks != null) {
+            callbacks.onResponse(msg.content);
+        }
     }
 
     static class UnityMessageReceiver {
