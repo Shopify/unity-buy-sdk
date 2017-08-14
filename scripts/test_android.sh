3d66fe4ef171024e18923c909ce374ad1a68d60a
Jeffrey Deng <jeffreydeng@live.com>
2017-08-14 10:12:56 -0400

    [Refactor] Refactored test scripts to fix lint issues (#276)

--- scripts/test_android.sh
+++ scripts/test_android.sh
@@ -1,5 +1,7 @@
-. $(dirname $0)/common.sh
+#!/bin/bash
 
-ANDROID_PLUGIN_ROOT="$PROJECT_ROOT/android"
+. "$(dirname "$0")"/common.sh
+
+ANDROID_PLUGIN_ROOT="$PROJECT_ROOT"/android
 
 "$ANDROID_PLUGIN_ROOT"/gradlew shopify_buy_plugin:check -p "$ANDROID_PLUGIN_ROOT" || die "Failed to check Android plugin"
