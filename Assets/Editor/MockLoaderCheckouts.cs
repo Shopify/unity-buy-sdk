3381e6eb96422acf060cedb9196cc8b0a0943bd6
Richard Monette <richard.monette@shopify.com>
2017-08-14 14:30:05 -0400

    update checkout query

--- Assets/Editor/MockLoaderCheckouts.cs
+++ Assets/Editor/MockLoaderCheckouts.cs
@@ -1,19 +1,19 @@
 namespace Shopify.Tests {
     public class MockLoaderCheckouts : BaseMockLoader {
         // The following query is for a straightforward checkout create
-        private const string CREATE_QUERY = @"mutation{checkoutCreate (input:{allowPartialAddresses:true,lineItems:[{quantity:33,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTE1NQ==""},{quantity:2,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTM0Nw==""}]}){checkout {id webUrl currencyCode requiresShipping subtotalPrice totalTax totalPrice ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
+        private const string CREATE_QUERY = @"mutation{checkoutCreate (input:{lineItems:[{quantity:33,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTE1NQ==""},{quantity:2,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTM0Nw==""}],allowPartialAddresses:true}){checkout {id webUrl currencyCode requiresShipping subtotalPrice totalTax totalPrice ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
 
         // The following two queries are used for a test where first a create is called and we return false for ready which forces a poll
         // then we return a result that ready is true and the new weburl is there
-        private const string CREATE_QUERY_THAT_POLLS = @"mutation{checkoutCreate (input:{allowPartialAddresses:true,lineItems:[{quantity:33,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTPOLL==""},{quantity:2,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTM0Nw==""}]}){checkout {id webUrl currencyCode requiresShipping subtotalPrice totalTax totalPrice ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
+        private const string CREATE_QUERY_THAT_POLLS = @"mutation{checkoutCreate (input:{lineItems:[{quantity:33,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTPOLL==""},{quantity:2,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTM0Nw==""}],allowPartialAddresses:true}){checkout {id webUrl currencyCode requiresShipping subtotalPrice totalTax totalPrice ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
         private const string POLL_QUERY_AFTER_CREATE = @"{node (id:""checkout-id-poll""){__typename ...on Checkout{id webUrl currencyCode requiresShipping subtotalPrice totalTax totalPrice ready }}}";
 
         // The following queries are used to test creating a checkout, then modififying line items and getting a new url
-        private const string CREATE_QUERY_THAT_WILL_BE_UPDATED = @"mutation{checkoutCreate (input:{allowPartialAddresses:true,lineItems:[{quantity:33,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate==""},{quantity:2,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyDelete==""}]}){checkout {id webUrl currencyCode requiresShipping subtotalPrice totalTax totalPrice ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
+        private const string CREATE_QUERY_THAT_WILL_BE_UPDATED = @"mutation{checkoutCreate (input:{lineItems:[{quantity:33,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate==""},{quantity:2,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyDelete==""}],allowPartialAddresses:true}){checkout {id webUrl currencyCode requiresShipping subtotalPrice totalTax totalPrice ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
         private const string ADD_UPDATE_DELETE_AFTER_CREATE = @"mutation{checkoutLineItemsRemove (checkoutId:""checkout-id-poll"",lineItemIds:[""line-item-id1"",""line-item-id2""]){userErrors {field message }}checkoutLineItemsAdd (lineItems:[{quantity:10,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate==""},{quantity:3,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NTMNewItem==""}],checkoutId:""checkout-id-poll""){checkout {id webUrl currencyCode requiresShipping subtotalPrice totalTax totalPrice ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
 
         // The following query will return a user error
-        private const string CREATE_QUERY_SENDS_USERERROR = @"mutation{checkoutCreate (input:{allowPartialAddresses:true,lineItems:[{quantity:1,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWNFyaWFudC8yMDc1NjEyUserError==""}]}){checkout {id webUrl currencyCode requiresShipping subtotalPrice totalTax totalPrice ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
+        private const string CREATE_QUERY_SENDS_USERERROR = @"mutation{checkoutCreate (input:{lineItems:[{quantity:1,variantId:""Z2lkOi8vc2hvcGlmeS9Qcm9kdWNFyaWFudC8yMDc1NjEyUserError==""}],allowPartialAddresses:true}){checkout {id webUrl currencyCode requiresShipping subtotalPrice totalTax totalPrice ready lineItems (first:250){edges {node {id variant {id }}cursor }pageInfo {hasNextPage }}}userErrors {field message }}}";
 
         // The following queries will return the completedAt date for a checkout for validation tesitng
         private const string QUERY_COMPLETED_AT_WITH_DATE = @"{node (id:""checkout-id""){__typename ...on Checkout{completedAt }}}";
@@ -32,12 +32,12 @@ namespace Shopify.Tests {
 
         private void SetupCreateResponse() {
             AddResponse(
-                CREATE_QUERY, 
+                CREATE_QUERY,
                 @"{
                     ""data"": {
                         ""checkoutCreate"": {
                             ""checkout"": {
-                                ""id"": ""checkout-id"", 
+                                ""id"": ""checkout-id"",
                                 ""webUrl"": ""http://shopify.com/checkout-no-poll"",
                                 ""currencyCode"": ""CAD"",
                                 ""requiresShipping"": true,
@@ -49,7 +49,7 @@ namespace Shopify.Tests {
                                     ""edges"": [
                                         {
                                             ""node"": {
-                                                ""id"": ""line-item-id1"", 
+                                                ""id"": ""line-item-id1"",
                                                 ""variant"": {
                                                     ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTE1NQ==""
                                                 }
@@ -58,19 +58,19 @@ namespace Shopify.Tests {
                                         },
                                         {
                                             ""node"": {
-                                                ""id"": ""line-item-id2"", 
+                                                ""id"": ""line-item-id2"",
                                                 ""variant"": {
                                                     ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTM0Nw==""
                                                 }
                                             },
                                             ""cursor"": ""line-item-1-cursor""
-                                        } 
+                                        }
                                     ],
                                     ""pageInfo"": {
                                         ""hasNextPage"": false
                                     }
                                 }
-                            }, 
+                            },
                             ""userErrors"": []
                         }
                     }
@@ -80,12 +80,12 @@ namespace Shopify.Tests {
 
         private void SetupCreateResponsesForPolling() {
             AddResponse(
-                CREATE_QUERY_THAT_POLLS, 
+                CREATE_QUERY_THAT_POLLS,
                 @"{
                     ""data"": {
                         ""checkoutCreate"": {
                             ""checkout"": {
-                                ""id"": ""checkout-id-poll"", 
+                                ""id"": ""checkout-id-poll"",
                                 ""webUrl"": ""http://shopify.com/checkout-with-poll"",
                                 ""currencyCode"": ""CAD"",
                                 ""requiresShipping"": true,
@@ -97,7 +97,7 @@ namespace Shopify.Tests {
                                     ""edges"": [
                                         {
                                             ""node"": {
-                                                ""id"": ""line-item-id1"", 
+                                                ""id"": ""line-item-id1"",
                                                 ""variant"": {
                                                     ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTPOLL==""
                                                 }
@@ -106,19 +106,19 @@ namespace Shopify.Tests {
                                         },
                                         {
                                             ""node"": {
-                                                ""id"": ""line-item-id2"", 
+                                                ""id"": ""line-item-id2"",
                                                 ""variant"": {
                                                     ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyOTM0Nw==""
                                                 }
                                             },
                                             ""cursor"": ""line-item-1-cursor""
-                                        } 
+                                        }
                                     ],
                                     ""pageInfo"": {
                                         ""hasNextPage"": false
                                     }
                                 }
-                            }, 
+                            },
                             ""userErrors"": []
                         }
                     }
@@ -131,8 +131,8 @@ namespace Shopify.Tests {
                     ""data"": {
                         ""node"": {
                             ""__typename"": ""Checkout"",
-                            ""id"": ""checkout-id-poll"", 
-                            ""webUrl"": ""http://shopify.com/checkout-with-poll-after-poll"", 
+                            ""id"": ""checkout-id-poll"",
+                            ""webUrl"": ""http://shopify.com/checkout-with-poll-after-poll"",
                             ""ready"": true
                         }
                     }
@@ -147,7 +147,7 @@ namespace Shopify.Tests {
                     ""data"": {
                         ""checkoutCreate"": {
                             ""checkout"": {
-                                ""id"": ""checkout-id-poll"", 
+                                ""id"": ""checkout-id-poll"",
                                 ""webUrl"": ""http://shopify.com/checkout-create-before-update"",
                                 ""currencyCode"": ""CAD"",
                                 ""requiresShipping"": true,
@@ -159,7 +159,7 @@ namespace Shopify.Tests {
                                     ""edges"": [
                                         {
                                             ""node"": {
-                                                ""id"": ""line-item-id1"", 
+                                                ""id"": ""line-item-id1"",
                                                 ""variant"": {
                                                     ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate==""
                                                 }
@@ -168,25 +168,25 @@ namespace Shopify.Tests {
                                         },
                                         {
                                             ""node"": {
-                                                ""id"": ""line-item-id2"", 
+                                                ""id"": ""line-item-id2"",
                                                 ""variant"": {
                                                     ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyDelete==""
                                                 }
                                             },
                                             ""cursor"": ""line-item-1-cursor""
-                                        } 
+                                        }
                                     ],
                                     ""pageInfo"": {
                                         ""hasNextPage"": false
                                     }
                                 }
-                            }, 
+                            },
                             ""userErrors"": []
                         }
                     }
                 }"
             );
-            
+
             AddResponse(
                 ADD_UPDATE_DELETE_AFTER_CREATE,
                 @"{
@@ -210,7 +210,7 @@ namespace Shopify.Tests {
                                             ""node"": {
                                                 ""id"": ""line-item-id1"",
                                                 ""variant"": {
-                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate=="" 
+                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NjEyUpdate==""
                                                 }
                                             },
                                             ""cursor"": ""line-item-1-cursor""
@@ -219,14 +219,14 @@ namespace Shopify.Tests {
                                             ""node"": {
                                                 ""id"": ""line-item-id3"",
                                                 ""variant"": {
-                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NTMNewItem=="" 
+                                                    ""id"": ""Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yMDc1NTMNewItem==""
                                                 }
                                             },
                                             ""cursor"": ""line-item-3-cursor""
                                         }
                                     ],
                                     ""pageInfo"": {
-                                        ""hasNextPage"": false 
+                                        ""hasNextPage"": false
                                     }
                                 }
                             },
