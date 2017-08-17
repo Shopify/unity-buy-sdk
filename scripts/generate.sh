2f8d8a12f9b858debb0993e0063959e70ce30c79
Jeffrey Deng <jeffreydeng@live.com>
2017-08-17 17:52:14 -0400

    Cleanup

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,9 +3,6 @@
 bundle --gemfile="$SCRIPTS_ROOT"/generator/Gemfile
 "$SCRIPTS_ROOT"/generator/update_schema "$1"
 
-bundle --gemfile="$SCRIPTS_ROOT"/generator/Gemfile
-"$SCRIPTS_ROOT"/generator/update_schema
-
 UPDATE_SCHEMA_RESULT=$?
 
 if [[ $UPDATE_SCHEMA_RESULT = 0 ]] ; then
