303738ce5e775417f7e6cd37487d9b822e263e63
Jeffrey Deng <jeffreydeng@live.com>
2017-08-22 10:32:05 -0400

    [Refactor] General scripts (#287)
    * Refactored scripts to fix majority of the linting issues

--- scripts/build_documentation.sh
+++ scripts/build_documentation.sh
@@ -1,13 +1,16 @@
-. $(dirname $0)/common.sh
+#!/bin/bash
+
+. "$(dirname "$0")"/common.sh
 
 which doxygen &> /dev/null || die "doxygen is not installed"
 
-cat $PROJECT_ROOT/scripts/docs/temp_landing.md > $PROJECT_ROOT/scripts/docs/landing.md
-cat $PROJECT_ROOT/EXAMPLES.md >> $PROJECT_ROOT/scripts/docs/landing.md
+cat "$PROJECT_ROOT"/scripts/docs/temp_landing.md > "$PROJECT_ROOT"/scripts/docs/landing.md
+cat "$PROJECT_ROOT"/EXAMPLES.md >> "$PROJECT_ROOT"/scripts/docs/landing.md
 
 doxygen Doxyfile
+DOC_RESULT=$?
 
-if [ $? = 0 ] ; then
+if [[ $DOC_RESULT = 0 ]] ; then
     echo "🦄  Generated documentation 🦄"
     exit 0
 else
