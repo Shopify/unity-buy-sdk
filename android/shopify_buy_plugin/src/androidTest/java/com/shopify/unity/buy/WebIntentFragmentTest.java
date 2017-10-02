8c470e11d553fa5d41ab2804ad2a9c8cc7dcf8a4
Stephan Leroux <stephan.leroux@shopify.com>
2017-10-02 11:35:36 -0400

    Fixed Android instrumentation tests and added connectedAndroidTest to test_android script

--- android/shopify_buy_plugin/src/androidTest/java/com/shopify/unity/buy/WebIntentFragmentTest.java
+++ android/shopify_buy_plugin/src/androidTest/java/com/shopify/unity/buy/WebIntentFragmentTest.java
@@ -1,31 +1,47 @@
 package com.shopify.unity.buy;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.test.filters.MediumTest;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Rule;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @MediumTest
 @RunWith(AndroidJUnit4.class)
 public class WebIntentFragmentTest implements WebIntentListener {
-
-    @Rule
-    public ActivityTestRule<MockUnityActivity> rule =
-        new ActivityTestRule<>(MockUnityActivity.class);
-
     private boolean hasClosed = false;
+    private Activity mockActivity;
+
+    @Before
+    public void setUp() throws Exception {
+        this.mockActivity = Mockito.mock(Activity.class);
+        when(this.mockActivity.getPackageName()).thenReturn("com.shopify.unity.buy.test");
+
+        FragmentManager mockFragmentManager = Mockito.mock(FragmentManager.class);
+        FragmentTransaction mockFragmentTransaction = Mockito.mock(FragmentTransaction.class);
+
+        when(mockFragmentManager.beginTransaction()).thenReturn(mockFragmentTransaction);
+        when(mockFragmentTransaction.add(any(Fragment.class), anyString())).thenReturn(mockFragmentTransaction);
+        when(this.mockActivity.getFragmentManager()).thenReturn(mockFragmentManager);
+    }
 
     @Test
     public void testListener() {
         WebIntentFragment webFragment = WebIntentFragment.newInstance("http://shopify.com");
         webFragment.setListener(this);
-        rule.getActivity().getFragmentManager().beginTransaction().add(webFragment, "").commit();
+        mockActivity.getFragmentManager().beginTransaction().add(webFragment, "").commit();
         assertFalse(hasClosed);
 
         webFragment.onPause();
--- android/shopify_buy_plugin/src/main/java/com/shopify/unity/buy/MockUnityActivity.java
+++
@@ -1,5 +0,0 @@
-package com.shopify.unity.buy;
-
-import android.app.Activity;
-
-public class MockUnityActivity extends Activity { }
