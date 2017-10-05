b3e767bad9c9f96c5d76c4ee544923651aef9296
Stephan Leroux <stephan.leroux@shopify.com>
2017-10-05 11:29:18 -0400

    [Android] Fix tests and add Android CI on Travis (#312)
    * Fixed Android instrumentation tests and added connectedAndroidTest to test_android script
    * Added .travis.yml for Android CI
    
    * Fixed up travis yml

--- scripts/test_android.sh
+++ scripts/test_android.sh
@@ -4,4 +4,6 @@
 
 ANDROID_PLUGIN_ROOT="$PROJECT_ROOT"/android
 
-"$ANDROID_PLUGIN_ROOT"/gradlew shopify_buy_plugin:check -p "$ANDROID_PLUGIN_ROOT" || die "Failed to check Android plugin"
+"$ANDROID_PLUGIN_ROOT"/gradlew shopify_buy_plugin:check -p "$ANDROID_PLUGIN_ROOT" || die "Failed to run unit tests for Android plugin"
+
+"$ANDROID_PLUGIN_ROOT"/gradlew shopify_buy_plugin:connectedAndroidTest -p "$ANDROID_PLUGIN_ROOT" || die "Failed to run instrumentation tests for Android plugin"
