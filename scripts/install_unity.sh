07e3fd06994fa39e33ee7149c77489fe7d3c26b6
Stephan Leroux <stephan.leroux@shopify.com>
2017-10-05 14:53:43 -0400

    Updated CI Unity version to 2017.1.1f1 because of Xcode 9 issues

--- scripts/install_unity.sh
+++ scripts/install_unity.sh
@@ -5,9 +5,9 @@
 # set UNITY_DOWNLOAD_DIR if it hasn't been set. It may have been set from 
 UNITY_DOWNLOAD_DIR="${UNITY_DOWNLOAD_DIR:-`pwd`/unity}"
 UNITY_PKG_LOCATION=${UNITY_PKG_LOCATION:-"$UNITY_DOWNLOAD_DIR"/Unity.pkg}
-UNITY_PKG_URL=${UNITY_PKG_URL:-http://netstorage.unity3d.com/unity/3829d7f588f3/MacEditorInstaller/Unity-5.5.2f1.pkg}
+UNITY_PKG_URL=${UNITY_PKG_URL:-http://netstorage.unity3d.com/unity/3829d7f588f3/MacEditorInstaller/Unity-2017.1.1f1.pkg}
 IOS_PKG_LOCATION=${IOS_PKG_LOCATION:-"$UNITY_DOWNLOAD_DIR"/Unity-iOS.pkg}
-IOS_PKG_URL=${IOS_PKG_URL:-http://netstorage.unity3d.com/unity/3829d7f588f3/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-5.5.2f1.pkg}
+IOS_PKG_URL=${IOS_PKG_URL:-http://netstorage.unity3d.com/unity/3829d7f588f3/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-2017.1.1f1.pkg}
 
 if [[ ! -e $UNITY_PKG_LOCATION ]] ; then
     out "Downloading Unity to $UNITY_DOWNLOAD_DIR"
