30886baa63b79d5217ea36f6eb8c8f6d54ddfe55
Stephan Leroux <stephan.leroux@shopify.com>
2017-10-11 14:08:02 -0400

    [Fixes] Various fixes for building iOS/Android platforms (#334)
    * Fixed broken path for SwiftInterfaceHeader
    * Removed duplicated .AAR library
    
    * Moved Android libs to proper Plugins folder and removed extra folder
    
    * Updated PlayServicesResolver and use new .XML method instead of .CS imports
    
    * Moved ShopifyDependencies.xml into Editor folder under Plugins
    
    * Added in play-services resolver

--- scripts/generator/graphql_generator/csharp.rb
+++ scripts/generator/graphql_generator/csharp.rb
@@ -95,7 +95,6 @@ module GraphQLGenerator
         CartState
         Editor/ShopifyOnboardingPanel
         Editor/ShopifyEditorStyleHelper
-        Editor/ShopifyDependencies
         Editor/BuildPipeline/iOSPostProcessor
         Editor/BuildPipeline/ExtendedPBXProject
         SDK/Arguments
