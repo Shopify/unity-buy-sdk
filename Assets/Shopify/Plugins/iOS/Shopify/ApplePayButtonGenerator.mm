27c9583459a73a5dbac5a24462b2707ee9d5b423
Stephan Leroux <stephan.leroux@shopify.com>
2017-10-16 14:29:54 -0400

    More documentation and adding of enum/memory management fix

--- Assets/Shopify/Plugins/iOS/Shopify/ApplePayButtonGenerator.mm
+++ Assets/Shopify/Plugins/iOS/Shopify/ApplePayButtonGenerator.mm
@@ -34,6 +34,7 @@ extern "C" {
         }
     }
     
+    // Helper method for making a copy of a C String.
     const char* cStringCopy(const char* string) {
         if (string == NULL)
             return NULL;
@@ -44,26 +45,40 @@ extern "C" {
         return res;
     }
     
-    const char* _GenerateApplePayButtonImage(const char *type, const char* style, float width, float height) {
+    // Generates a base64 encoded string containing the rendered image output of a PKPaymentButton from iOS.
+    const char* _GenerateApplePayButtonImage(const char *type, const char* style, float width, float height, bool includeMinMargin) {
         PKPaymentButton* button = [PKPaymentButton buttonWithType:SBPayButtonTypeFromString(type)
                                                             style:SBPayButtonStyleFromString(style)];
         CGSize size = CGSizeMake(width, height);
-        button.frame = CGRectMake(0, 0, size.width, size.height);
+                       
+        // According to Apple Design guidelines, there must be a minimum spacing around the Apple Pay button of at least 1/10 the height.
+        // We include this as an optional property when rendering the button image.
+        CGFloat minSpace = 0;
+        if (false) {
+            minSpace = 0.10 * height;
+            button.frame = CGRectMake(0, 0, size.width - 2 * minSpace, size.height - 2 * minSpace);
+        } else {
+            button.frame = CGRectMake(0, 0, size.width, size.height);
+        }
         
         UnityAppController* unityController = (UnityAppController*)UIApplication.sharedApplication.delegate;
         [unityController.rootView insertSubview:button atIndex:0];
         
-        // Take snapshot of the Apple Pay button
+        // Take snapshot of the Apple Pay button.
         UIGraphicsBeginImageContextWithOptions(size, NO, [UIScreen mainScreen].scale);
         CGContextRef context = UIGraphicsGetCurrentContext();
+        CGContextTranslateCTM(context, minSpace, minSpace);
         [button.layer renderInContext:context];
         UIImage* snapshot = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
         
         [button removeFromSuperview];
         
-        // Convert to bytes and pass to Unity
+        // Convert to bytes and pass to Unity.
         NSString *byteArray = [UIImagePNGRepresentation(snapshot) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+        
+        // We create a malloc'ed copy of the string here so we can manually release the memory associated
+        // with it on the Unity side using Marshal.FreeHGlobal.
         return cStringCopy([byteArray UTF8String]);
     }
 #ifdef __cplusplus
