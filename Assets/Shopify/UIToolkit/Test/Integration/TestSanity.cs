a74f75bf0343c073e37b573efd4175e43a83078d
Jake Cataford <jakecataford@gmail.com>
2017-11-15 11:00:05 -0500

    Create sanity checks + folder structure for ui toolkit

---
+++ Assets/Shopify/UIToolkit/Test/Integration/TestSanity.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopify.UIToolkit.Test.Integration {
+	using UnityEngine;
+
+	[IntegrationTest.DynamicTest("UIToolkitIntegrationTests")]
+    public class TestSanity : MonoBehaviour {
+        public void Start() {
+            IntegrationTest.Pass();
+        }
+    }
+}
