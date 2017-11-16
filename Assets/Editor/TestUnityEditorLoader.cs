d3417c42fde0435da93ba1f1c1721fcb064f21ac
Jake Cataford <jakecataford@gmail.com>
2017-11-16 14:48:41 -0500

    Create Unity Editor Loader
    This is a loader class that handles loading when the normal game loop is
    not active. It binds to the EditorApplication.update event and manually
    ticks the www coroutine forward to complete the request.

---
+++ Assets/Editor/TestUnityEditorLoader.cs
@@ -0,0 +1,33 @@
+﻿namespace Shopify.Tests {
+    using NUnit.Framework;
+    using UnityEngine.TestTools;
+    using UnityEngine;
+    using Shopify.Unity.SDK.Editor;
+    using Shopify.Unity.SDK;
+    using System.Collections;
+
+    [TestFixture]
+    public class TestUnityEditorLoader {
+        [UnityTest]
+        public IEnumerator TestWithValidQuery() {
+            var requestIsFinished = false;
+
+            string domain = "graphql.myshopify.com";
+            string authorization = "351c122017d0f2a957d32ae728ad749c";
+
+            var queryLoader = new QueryLoader(new UnityEditorLoader(domain, authorization));
+
+            queryLoader.Query(TestQueries.Query, (QueryResponse response) => {
+                Assert.True(response.HTTPError == null, "http errors were not null: " + response.HTTPError);
+                Assert.True(response.errors == null, "GraphQL errors were null");
+                Assert.True(response.data != null, "Received a response");
+                Assert.True(response.data.shop().name() != null, "graphql");
+                requestIsFinished = true;
+            });
+
+            while (!requestIsFinished) {
+                yield return null;
+            }
+        }
+    }
+}
