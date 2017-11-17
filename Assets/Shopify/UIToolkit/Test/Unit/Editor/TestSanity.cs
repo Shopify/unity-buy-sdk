fdc0dd748868fb75a6abc28aeedf70425abf840b
Jake Cataford <jake.cataford@shopify.com>
2017-11-17 10:39:10 -0500

    Create sanity checks + folder structure for ui toolkit (#371)

---
+++ Assets/Shopify/UIToolkit/Test/Unit/Editor/TestSanity.cs
@@ -0,0 +1,11 @@
+﻿namespace Shopify.UIToolkit.Test.Unit {
+	using NUnit.Framework;
+
+    [TestFixture]
+    public class TestSanity {
+		[Test]
+		public void TestShouldPass() {
+            Assert.True(true);
+        }
+    }
+}
