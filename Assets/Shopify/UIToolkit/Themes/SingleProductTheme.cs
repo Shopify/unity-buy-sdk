79091cdedcbf8f6018b5304f0d29912597343e3e
Jake Cataford <jakecataford@gmail.com>
2017-11-21 14:40:18 -0500

    [UI Toolkit] Scaffold Single Product Theme
    Provides the scaffolding and docs for a single product theme

---
+++ Assets/Shopify/UIToolkit/Themes/SingleProductTheme.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopify.UIToolkit {
+    using Shopify.Unity;
+
+    /// <summary>
+    /// Override this class to create a theme that sells a single product
+    /// </summary>
+    public abstract class SingleProductTheme : ThemeBase {
+        /// <summary>
+        /// Called when the theme should show information about the product it's selling
+        /// </summary>
+        /// <param name="product">The product that should be displayed</param>
+        /// <param name="variants">The variants of the product that should be displayed</param>
+        public abstract void OnShouldShowProduct(Product product, ProductVariant[] variants);
+    }
+}
