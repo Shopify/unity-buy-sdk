136e32aafd514ec01a13dd632b465f75af41e616
Jake Cataford <jake.cataford@shopify.com>
2017-11-22 15:00:54 -0500

    [UI Toolkit] Create base theme controller class (#375)
    Creates a base theme controller that contains the access token and shop
    domain, as well as the ability to initialize a client.

---
+++ Assets/Shopify/UIToolkit/ThemeControllerBase.cs
@@ -0,0 +1,34 @@
+﻿namespace Shopify.UIToolkit {
+    using UnityEngine;
+    using Shopify.Unity;
+
+    public abstract class ThemeControllerBase : MonoBehaviour {
+        /// <summary>
+        /// The Shop Domain to connect to, in the form of "myshop.myshopify.com"
+        /// </summary>
+        public string ShopDomain;
+
+        /// <summary>
+        /// The Storefront Access Token used to authenticate with the connected shop
+        /// </summary>
+        public string AccessToken;
+
+        /// <summary>
+        /// The client that the controller is using to make requests against the SDK.
+        /// </summary>
+        /// <returns></returns>
+        public ShopifyClient Client {
+            get {
+                if (_cachedClient != null &&
+                    _cachedClient.AccessToken == AccessToken && 
+                    _cachedClient.Domain == ShopDomain) {
+                        return _cachedClient;
+                }
+
+                _cachedClient = new ShopifyClient(AccessToken, ShopDomain);
+                return _cachedClient;
+            }
+        }
+        private ShopifyClient _cachedClient;
+    }
+}
