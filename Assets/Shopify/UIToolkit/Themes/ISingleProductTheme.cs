9d8449e1e966abbcd589ee0b051bf2dc717bc5b6
Jake Cataford <jakecataford@gmail.com>
2017-11-29 14:45:21 -0500

    [UI Toolkit] Drop abstract theme classes in favor of interfaces
    Interfaces have a clearer separation of concerns and are more flexible
    than abstract classes for our themes. Migrating them doesn't really
    change the game plan so we might as well do it!

--- Assets/Shopify/UIToolkit/Themes/ISingleProductTheme.cs
+++ Assets/Shopify/UIToolkit/Themes/ISingleProductTheme.cs
@@ -4,12 +4,12 @@
     /// <summary>
     /// Override this class to create a theme that sells a single product
     /// </summary>
-    public abstract class SingleProductTheme : ThemeBase, ISingleProductTheme {
+    public interface ISingleProductTheme : IThemeBase {
         /// <summary>
         /// Called when the theme should show information about the product it's selling
         /// </summary>
         /// <param name="product">The product that should be displayed</param>
         /// <param name="variants">The variants of the product that should be displayed</param>
-        public abstract void OnShouldShowProduct(Product product, ProductVariant[] variants);
+        void OnShouldShowProduct(Product product, ProductVariant[] variants);
     }
 }
--- Assets/Shopify/UIToolkit/Themes/Interfaces/ISingleProductTheme.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shopify.UIToolkit {
-    using Shopify.Unity;
-
-    public interface ISingleProductTheme : IThemeBase {
-        void OnShouldShowProduct(Product product, ProductVariant[] variants);
-    }
-}
