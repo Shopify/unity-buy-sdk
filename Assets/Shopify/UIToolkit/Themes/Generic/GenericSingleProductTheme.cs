665aaf3957472e55f5a61008a214dadbcc755178
Jake Cataford <jake.cataford@shopify.com>
2017-12-11 11:54:44 -0500

    [UI Toolkit] Creates prefab + layout for generic single product theme (#415)

---
+++ Assets/Shopify/UIToolkit/Themes/Generic/GenericSingleProductTheme.cs
@@ -0,0 +1,36 @@
+﻿namespace Shopify.UIToolkit.Themes {
+    using Shopify.Unity;
+    using Shopify.Unity.SDK;
+    using UnityEngine;
+
+    [RequireComponent(typeof(SingleProductThemeController))]
+    public class GenericSingleProductTheme : MonoBehaviour, ISingleProductTheme {
+        void IThemeBase.OnError(ShopifyError error) {
+            throw new System.NotImplementedException();
+        }
+
+        void IThemeBase.OnLoadingFinished() {
+            throw new System.NotImplementedException();
+        }
+
+        void IThemeBase.OnLoadingStarted() {
+            throw new System.NotImplementedException();
+        }
+
+        void IThemeBase.OnPurchaseCancelled() {
+            throw new System.NotImplementedException();
+        }
+
+        void IThemeBase.OnPurchaseCompleted() {
+            throw new System.NotImplementedException();
+        }
+
+        void IThemeBase.OnPurchaseStarted() {
+            throw new System.NotImplementedException();
+        }
+
+        void ISingleProductTheme.OnShouldShowProduct(Product product, ProductVariant[] variants) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
