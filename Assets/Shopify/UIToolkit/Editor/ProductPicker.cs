b8163fd04701b2ea286d567f0d3777c541f78fda
Jake Cataford <jake.cataford@shopify.com>
2017-12-12 10:37:07 -0500

    [UI Toolkit] Editor Utility for showing a product picker in the editor (#400)
    * [UI Toolkit] Editor Utility for showing a product picker in the editor
    
    Implementation of a tool that is to be called within an editor script
    that allows the loading and rendering of a product picker within the
    editor.
    
    Follows a view/controller pattern that allows us to mock EditorGUI calls
    out for tests.
    
    * Better separate concerns of view and controller, add pagination

---
+++ Assets/Shopify/UIToolkit/Editor/ProductPicker.cs
@@ -0,0 +1,168 @@
+﻿namespace Shopify.UIToolkit.Editor {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEditor;
+    using Shopify.Unity;
+    using Shopify.Unity.SDK;
+    using System;
+    using System.Linq;
+
+    public interface IProductPickerView {
+        void DrawProductLoadingState(SerializedObject serializedObject);
+        void DrawProductPicker(SerializedObject serializedObject, Dictionary<string, string> productNameToGIDMap);
+        void DrawProductLoadingError(SerializedObject serializedObject);
+        void DrawShopHasNoProductsState(SerializedObject serializedObject);
+    }
+
+    public interface IProductPickerController {
+        void OnShouldRefreshProductList();
+    }
+
+    public class ProductPicker : IProductPickerController {
+        private IProductPickerView _view;
+        private static Dictionary<string, string> _cachedProductNameToGIDMap;
+        private Dictionary<string, string> _productNameToGIDMap;
+        private ShopifyClient _client;
+        private ShopifyError _error;
+
+        public bool ProductsFinishedLoading() {
+            return _productNameToGIDMap != null || _error != null;
+        }
+
+        public ProductPicker(ShopifyClient client, IProductPickerView view = null) {
+            _view = view ?? new ProductPickerView(this);
+            _client = client;
+            LoadProducts();
+        }
+
+        public void DrawInspectorGUI(SerializedObject serializedObject) {
+            if (_error != null) {
+                _view.DrawProductLoadingError(serializedObject);
+                return;
+            }
+
+            if (!ProductsFinishedLoading()) {
+                _view.DrawProductLoadingState(serializedObject);
+                return;
+            }
+
+            if (_productNameToGIDMap.Count == 0) {
+                _view.DrawShopHasNoProductsState(serializedObject);
+                return;
+            }
+
+            _view.DrawProductPicker(serializedObject, _productNameToGIDMap);
+        }
+
+        public void LoadProducts() {
+            if (_cachedProductNameToGIDMap != null) {
+                _productNameToGIDMap = _cachedProductNameToGIDMap;
+            }
+
+            LoadAllProducts();
+        }
+
+        public void OnShouldRefreshProductList() {
+            RefreshProducts();
+        }
+
+        private void LoadAllProducts(string lastPageAfter = null, List<Product> allProducts = null) {
+            allProducts = allProducts ?? new List<Product>();
+
+            _client.products((products, error, after) => {
+                if (error != null) {
+                    OnFailedToLoadProducts(error);
+                    return;
+                }
+
+                if (after == null) {
+                    OnLoadedProducts(products);
+                    return;
+                }
+
+                allProducts.AddRange(products);
+                LoadAllProducts(after, allProducts);
+            }, after: lastPageAfter);
+        }
+
+        private void RefreshProducts() {
+            _error = null;
+            _productNameToGIDMap = null;
+            _cachedProductNameToGIDMap = null;
+            LoadProducts();
+        }
+
+        private void OnFailedToLoadProducts(ShopifyError error) {
+            _error = error;
+            _cachedProductNameToGIDMap = null;
+        }
+
+        private void OnLoadedProducts(List<Product> products) {
+            _productNameToGIDMap = new Dictionary<string, string>();
+
+            foreach(var product in products) {
+                _productNameToGIDMap[product.title()] = product.id();
+            }
+
+            _cachedProductNameToGIDMap = _productNameToGIDMap;
+        }
+    }
+
+    public class ProductPickerView : IProductPickerView {
+        private IProductPickerController _controller;
+
+        public ProductPickerView(IProductPickerController controller) {
+            _controller = controller;
+        }
+
+        public void DrawProductLoadingState(SerializedObject serializedObject) {
+            EditorGUILayout.LabelField("Loading Products...");
+        }
+
+        public void DrawProductLoadingError(SerializedObject serializedObject) {
+            EditorGUILayout.HelpBox("Could not load products from the connected shop.", MessageType.Error);
+
+            if (GUILayout.Button("Reload Products")) {
+                _controller.OnShouldRefreshProductList();
+            }
+        }
+
+        public void DrawShopHasNoProductsState(SerializedObject serializedObject) {
+            EditorGUILayout.HelpBox("The connected shop has no products published for the Shopify SDK.", MessageType.Error);
+
+            if (GUILayout.Button("Reload Products")) {
+                _controller.OnShouldRefreshProductList();
+            }
+        }
+
+        public void DrawProductPicker(SerializedObject serializedObject, Dictionary<string, string> productNameToGIDMap) {
+            GUILayout.Label("Product");
+            EditorGUILayout.BeginHorizontal();
+
+            var selectedProduct = productNameToGIDMap.FirstOrDefault((x) => {
+                    return serializedObject.FindProperty("ProductGID").stringValue == x.Value;
+            });
+
+            var productNameToGIDList = productNameToGIDMap.ToList();
+
+            var selectedIndex = productNameToGIDList.IndexOf(selectedProduct);
+            EditorGUI.BeginChangeCheck();
+
+            var newIndex = EditorGUILayout.Popup(
+                selectedIndex, 
+                productNameToGIDList.Select((x) => x.Key).ToArray()
+            );
+
+            if (EditorGUI.EndChangeCheck()) {
+                serializedObject.FindProperty("ProductGID").stringValue = productNameToGIDList[newIndex].Value;
+            }
+
+            if (GUILayout.Button("Refresh", EditorStyles.miniButton, GUILayout.ExpandWidth(false))) {
+                _controller.OnShouldRefreshProductList();
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Assets/Shopify/UIToolkit/Test/Unit/Editor/TestProductPicker.cs
@@ -0,0 +1,76 @@
+﻿namespace Shopify.UIToolkit.Test.Unit {
+    using System.Collections;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using Shopify.UIToolkit.Editor;
+    using Shopify.Unity;
+    using NSubstitute;
+    using UnityEngine;
+    using Shopify.Unity.SDK.Editor;
+    using UnityEditor;
+    using UnityEngine.TestTools;
+    using Shopify.Unity.SDK;
+
+    [TestFixture]
+    public class TestProductPicker  {
+        private const string AccessToken = "351c122017d0f2a957d32ae728ad749c";
+        private const string ShopDomain = "graphql.myshopify.com";
+        private IProductPickerView _view;
+        private ProductPicker _picker;
+        private ShopifyClient _client;
+
+        [SetUp]
+        public void Setup() {
+            _client = new ShopifyClient(new UnityEditorLoader(ShopDomain, AccessToken));
+            _view = Substitute.For<IProductPickerView>();
+            _picker = new ProductPicker(_client, _view);
+
+        }
+
+        [Test]
+        public void TestProductPickerRendersLoadingWhenNoProductsAvailable() {
+            _picker.OnShouldRefreshProductList();
+            _picker.DrawInspectorGUI(null);
+
+            _view.DidNotReceive().DrawProductLoadingError(null);
+            _view.DidNotReceive().DrawProductPicker(null, Arg.Any<Dictionary<string, string>>());
+            _view.DidNotReceive().DrawShopHasNoProductsState(null);
+            _view.Received().DrawProductLoadingState(null);
+        }
+
+        [UnityTest]
+        public IEnumerator TestProductPickerRendersProductPickerAfterLoadingProducts() {
+            _picker.OnShouldRefreshProductList();
+
+            while(!_picker.ProductsFinishedLoading()) {
+                yield return null;
+            }
+
+            _picker.DrawInspectorGUI(null);
+
+            _view.DidNotReceive().DrawProductLoadingState(null);
+            _view.DidNotReceive().DrawProductLoadingError(null);
+            _view.DidNotReceive().DrawShopHasNoProductsState(null);
+            _view.Received().DrawProductPicker(null, Arg.Any<Dictionary<string, string>>());
+        }
+
+        [UnityTest]
+        public IEnumerator TestProductPickerRendersErrorWhenError() {
+            _client = new ShopifyClient(new UnityEditorLoader(ShopDomain, "badbadbad"));
+            _view = Substitute.For<IProductPickerView>();
+            _picker = new ProductPicker(_client, _view);
+            _picker.OnShouldRefreshProductList();
+
+            while(!_picker.ProductsFinishedLoading()) {
+                yield return null;
+            }
+
+            _picker.DrawInspectorGUI(null);
+
+            _view.DidNotReceive().DrawProductPicker(null, Arg.Any<Dictionary<string, string>>());
+            _view.DidNotReceive().DrawProductLoadingState(null);
+            _view.DidNotReceive().DrawShopHasNoProductsState(null);
+            _view.Received().DrawProductLoadingError(null);
+        }
+    }
+}
