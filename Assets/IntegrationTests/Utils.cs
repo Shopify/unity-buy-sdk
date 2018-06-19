a178b0cc9abc2478244c4f0eb3f859026adbfa1f
Stephan Leroux <stephan.leroux@shopify.com>
2018-06-19 10:58:38 -0400

    Fixed timeout message and removed duplicated pico

--- Assets/IntegrationTests/Utils.cs
+++ Assets/IntegrationTests/Utils.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Shopify.Unity.Tests {
     public class Utils {
         public const float MaxQueryDuration = 3f;
-        public const string MaxQueryMessage = "Query failed to run in 1 seconds";
+        public const string MaxQueryMessage = "Query failed to run in 3 seconds";
 
         public static StoppableWaitForTime GetWaitQuery() {
             return new StoppableWaitForTime (MaxQueryDuration);
@@ -13,7 +13,6 @@ namespace Shopify.Unity.Tests {
 
         public static List<string> GetImageAliases() {
             return new List<string>() {
-                "pico",
                 "pico",
                 "icon",
                 "thumb",
