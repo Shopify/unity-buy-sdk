3e6e6f3e1c99223832abde78d61953b9ebcc5ba5
Stephan Leroux <stephan.leroux@shopify.com>
2018-10-17 13:48:38 -0400

    Moved Circle CI tests to Travis CI (excluding iOS)

--- scripts/install_unity.sh
+++ scripts/install_unity.sh
@@ -6,8 +6,6 @@
 UNITY_DOWNLOAD_DIR="${UNITY_DOWNLOAD_DIR:-`pwd`/unity}"
 UNITY_PKG_LOCATION=${UNITY_PKG_LOCATION:-"$UNITY_DOWNLOAD_DIR"/Unity.pkg}
 UNITY_PKG_URL=${UNITY_PKG_URL:-https://download.unity3d.com/download_unity/5d30cf096e79/MacEditorInstaller/Unity-2017.1.1f1.pkg}
-IOS_PKG_LOCATION=${IOS_PKG_LOCATION:-"$UNITY_DOWNLOAD_DIR"/Unity-iOS.pkg}
-IOS_PKG_URL=${IOS_PKG_URL:-http://netstorage.unity3d.com/unity/5d30cf096e79/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor.pkg}
 
 if [[ ! -e $UNITY_PKG_LOCATION ]] ; then
     out "Downloading Unity to $UNITY_DOWNLOAD_DIR"
@@ -15,13 +13,6 @@ if [[ ! -e $UNITY_PKG_LOCATION ]] ; then
     mkdir -p "$UNITY_DOWNLOAD_DIR"
     curl -o "$UNITY_PKG_LOCATION" "$UNITY_PKG_URL"
     out "Finished Downloading Unity"
-fi
-
-if [[ ! -e $IOS_PKG_LOCATION ]] ; then
-    out "Downloading iOS Editor Support to $UNITY_DOWNLOAD_DIR"
-    out "Downloading from {$IOS_PKG_URL}"
-    curl -o "$IOS_PKG_LOCATION" "$IOS_PKG_URL"
-    out "Finished Downloading iOS Editor Support"
 fi
 
 out "Start Install Unity"
@@ -32,14 +23,4 @@ if [[ $INSTALL_UNITY_RESULT = 0 ]] ; then
     out "Finished Install Unity"
 else
     die "Unable to install Unity"
-fi
-
-out "Start Install iOS Editor Support"
-sudo installer -dumplog -package "$IOS_PKG_LOCATION" -target /
-INSTALL_IOS_RESULT=$?
-
-if [[ $INSTALL_IOS_RESULT = 0 ]] ; then
-    out "Finished Install iOS Editor Support"
-else
-    die "Unable to install iOS Editor Support"
 fi
