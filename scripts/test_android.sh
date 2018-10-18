0f888fa69f4bf8fb6286523ffb1bf6e5239ab174
Stephan Leroux <stephan.leroux@shopify.com>
2018-10-18 13:49:15 -0400

    Flamed old test and import scripts for android that are no longer used

--- scripts/test_android.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. "$(dirname "$0")"/common.sh
-
-ANDROID_PLUGIN_ROOT="$PROJECT_ROOT"/android
-
-"$ANDROID_PLUGIN_ROOT"/gradlew shopify_buy_plugin:check -p "$ANDROID_PLUGIN_ROOT" || die "Failed to run unit tests for Android plugin"
-
-"$ANDROID_PLUGIN_ROOT"/gradlew shopify_buy_plugin:connectedAndroidTest -p "$ANDROID_PLUGIN_ROOT" || die "Failed to run instrumentation tests for Android plugin"
