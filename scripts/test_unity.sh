4d0df8548a03f4d88948813f612b3f3e99f161df
Jason Yin <jason.yin@shopify.com>
2021-01-15 10:32:02 -0500

    Updated unity version and pkg url for CI

--- scripts/test_unity.sh
+++ scripts/test_unity.sh
@@ -15,7 +15,10 @@ convertNUnitToJUnit() {
 }
 
 # UNITY_VERSION=$(ls "$UNITY_PACKAGE_MANAGER_PATH")
-printf "Testing with Unity Version: 2019.4.18f1"
+# printf "Testing with Unity Version: 2019.4.18f1"
+
+UNITY_VERSION=$(ls "$UNITY_PACKAGE_MANAGER_PATH")
+printf "Testing with Unity Version: %s\n" "$UNITY_VERSION"
 
 "$UNITY_PATH" \
     -batchmode \
